8c99c311ca8c6003ccf9d52e052c84c660dcbdb2
Vicente San Silvestre <vsansilvestre@growthaccelerationparters.com>
2015-06-22 14:41:11 -0600

    adding test 002 cases

--- scripts/test002/test002createtestdata.sql
+++ scripts/test002/test002createtestdata.sql
@@ -10,7 +10,7 @@ reportClientId INTEGER;
 reportClientName varchar DEFAULT 'TEST CLIENT 01';
 indicationId integer:=7; --Acute Lymphoblastic Leukemia
 drugIds INTEGER[] := ARRAY[156,160,2182, 3098, 3199, 3237, 3584];
-restrictionsIds INTEGER[]:= ARRAY[809,810,811,812,813,775,776,777,778,840,1865,1867];
+restrictionsIds INTEGER[]:= ARRAY[809,810,811,776,777,778,812,813,775,840,1865,1864,1867,841,842,2111,1882,1881,1880,1869,1860,2106,2172,1861];
 
 userEmail varchar DEFAULT 'vsansilvestre@growthaccelerationpartners.com';
 
@@ -40,12 +40,17 @@ group3Restrictions INTEGER[] :=ARRAY[812,813,775];
 --813 (Pharmacy) CT:1
 --775 (Medical) CT:1
 
-group4Name varchar DEFAULT 'TEST_002_GROUP_4';--this group contains only pharmacy steps criterias  (dim_restriction_type_id=1 and dim_criterion_type_id=2)
+group4Name varchar DEFAULT 'TEST_002_GROUP_4';--this group contains only pharmacy double steps criterias  (dim_restriction_type_id=1 and dim_criterion_type_id=2)
 group4Restrictions INTEGER[] :=ARRAY[840,1865,1867];
 --840 (Pharmacy) CT:2
 --1865 (Pharmacy) CT:2
 --1867 (Pharmacy) CT:2
 
+group5Name varchar DEFAULT 'TEST_002_GROUP_5';--this group contains pharmacy mixed(single , double, triple, quadruple) steps criterias
+group5Restrictions INTEGER[] :=ARRAY[840,1864,1865,841,842,2111,1882,1881,1880,1869];
+
+group6Name varchar DEFAULT 'TEST_002_GROUP_6';--this group contains pharmacy and medical Single steps criterias
+group6Restrictions INTEGER[] :=ARRAY[1860,2106,2172,1861];
 
 BEGIN
 
@@ -72,6 +77,12 @@ BEGIN
 
   --CREATE REPORT CUSTOM CRITERIA  GROUP FOR #4
   PERFORM createreportcriteriagroups(reportId,reportClientId,group4Restrictions,group4Name,userEmail);
+
+  --CREATE REPORT CUSTOM CRITERIA  GROUP FOR #5
+  PERFORM createreportcriteriagroups(reportId,reportClientId,group5Restrictions,group5Name,userEmail);
+
+  --CREATE REPORT CUSTOM CRITERIA  GROUP FOR #6
+  PERFORM createreportcriteriagroups(reportId,reportClientId,group6Restrictions,group6Name,userEmail);
   
 success:=true;
 RETURN success;
