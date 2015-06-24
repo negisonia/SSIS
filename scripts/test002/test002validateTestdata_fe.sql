2ab503898238b0b646fb0c49f203373a97b2f0ba
Vicente San Silvestre <vsansilvestre@growthaccelerationparters.com>
2015-06-24 16:38:10 -0600

    changes to test 002

---
+++ scripts/test002/test002validateTestdata_fe.sql
@@ -0,0 +1,31 @@
+﻿CREATE OR REPLACE FUNCTION test002validatetestdata() --FRONT END
+RETURNS boolean AS $$
+DECLARE
+  success boolean DEFAULT false;
+  reportId integer;
+  drugid integer;
+  restrictionid integer;
+  reportBussinesId varchar DEFAULT 'TEST REPORT 002';
+  reportName varchar DEFAULT 'TEST REPORT NAME 002';
+  drugIds INTEGER[] := ARRAY[156,160,2182, 3098, 3199, 3237, 3584];
+  restrictionsIds INTEGER[]:= ARRAY[809,810,811,776,777,778,812,813,775,840,1865,1864,1867,841,842,2111,1882,1881,1880,1869,1860,2106,2172,1861];  
+  groups VARCHAR[] :=ARRAY[group1Name,group2Name,group3Name,group4Name,group5Name,group6Name,group7Name,group8Name];
+
+BEGIN
+
+        SELECT validatereport(reportBussinesId,reportName,drugIds,restrictionsIds,groups);
+
+	--GET THE TEST REPORT ID
+	SELECT crr.report_id FROM criteria_restriction_reports crr WHERE crr.business_id=reportBussinesId and  crr.report_name=reportName INTO reportId;
+
+	--VALIDATE THAT THE TEST REPORT IS AVAILABLE IN THE FRONT END DATABASE
+	IF reportId != null THEN
+		--  VALIDATE REPORT GROUPS
+		PERFORM test002validategroups(reportId);
+       END IF;
+
+success:=true;
+RETURN success;	  
+	
+END
+$$ LANGUAGE plpgsql;
---
+++ scripts/utils/validateReportFe.sql
@@ -0,0 +1,50 @@
+﻿--VALIDATES THAT THE REPORT WITH THE SPECIFIES PARAMETER EXISTS IN THE FRONT END DATABASE
+CREATE OR REPLACE FUNCTION validatereport(reportBussinesId integer,reportName varchar,drugids integer[], restrictionids integer[], customgroups varchar[]) --FRONT END
+RETURNS boolean AS $$
+DECLARE
+  success boolean DEFAULT false;
+  reportId integer;
+  drugid integer;
+  restrictionid integer;
+  customgroup varchar;
+  
+BEGIN
+	--GET THE TEST REPORT ID
+	SELECT crr.report_id FROM criteria_restriction_reports crr WHERE crr.business_id=reportBussinesId and  crr.report_name=reportName INTO reportId;
+
+	--VALIDATE THAT THE TEST REPORT IS AVAILABLE IN THE FRONT END DATABASE
+	IF reportId = null THEN
+		SELECT throw_error('TEST REPORT DOES NOT EXISTS');
+	ELSE
+	
+		--VALIDATE THAT THE TEST REPORT DRUGS IN THE FRONT END DATABASE ARE THE SAME AS THE REPORT DRUGS IN THE ADMIN DATABASE
+		 FOREACH drugid IN ARRAY drugIds
+		 LOOP
+		   IF (SELECT EXISTS (SELECT 1 FROM criteria_restriction_drugs crd WHERE crd.report_id=reportId AND crd.drug_id=drugid) = false) THEN
+			select throw_error('TEST REPORT DRUG DOES NOT EXISTS');
+		   END IF;
+		 END LOOP;
+		
+		--VALIDATE THAT THE TEST REPORT RESTRICTIONS IN THE FRONT END DATABASE ARE THE SAME AS ADMIN  DATABASE
+		 FOREACH restrictionid IN ARRAY restrictionsIds
+		 LOOP
+		   IF (SELECT EXISTS (SELECT 1 FROM criteria_restriction_selection  crs WHERE crs.report_id=reportId and crs.dim_criteria_restriction_id=restrictionid) = false) THEN
+			select throw_error('TEST REPORT RESTRICTION DOES NOT EXISTS');
+		  END IF;
+		 END LOOP;
+
+		 --VALIDATE THAT THE TEST REPORT CUSTOM GROUPS EXISTS IN THE FRONT END DATABASE
+		 FOREACH customgroup IN ARRAY customgroups
+		 LOOP
+		   IF (SELECT EXISTS (SELECT 1 FROM custom_criteron_selection  ccs WHERE ccs.report_id=reportId and ccs.dim_criterion_type_id=3 and ccs.restriction_name=customgroup) = false) THEN
+			select throw_error('CUSTOM GROUP '|| customgroup ||' NOT EXISTS');
+		  END IF;
+		 END LOOP;		
+
+       END IF;
+
+success:=true;
+RETURN success;	  
+	
+END
+$$ LANGUAGE plpgsql;
