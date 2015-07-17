0f1f66d877db56ffc0a3d98fd75da287fb8c9d1f
lemaniak <lema017@gmail.com>
2015-07-17 11:52:40 -0600

    removing unnecesary parameter

--- Common/fingertip/common_cleanup.sql
+++ Common/fingertip/common_cleanup.sql
@@ -1,4 +1,4 @@
-﻿CREATE OR REPLACE FUNCTION clear_test_data(reportBussinessId varchar,reportName varchar, groupNames varchar[])--ADMIN
+﻿CREATE OR REPLACE FUNCTION clear_test_data()--ADMIN
 RETURNS boolean AS $$
 DECLARE
 success boolean DEFAULT false;
