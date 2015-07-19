41967f1e830be666c3d6b25fae43f1725a98e8f4
Ignacio <imonge@growthaccelerationpartners.com>
2015-07-19 14:02:12 -0600

    ANA-529 Create test data

---
+++ Common/analytics_front_end/clear_test_data.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION clear_test_data()--ANALYTICS FRONT END
+RETURNS boolean AS $$
+DECLARE
+success boolean DEFAULT false;
+BEGIN
+
+  --CLEAR TABLES
+  TRUNCATE TABLE criteria_reports_drugs CASCADE;
+  TRUNCATE TABLE criteria_reports_health_plan_types CASCADE;
+  TRUNCATE TABLE criteria_reports_markets CASCADE;
+  TRUNCATE TABLE criteria_reports CASCADE;
+  
+  --CLEAR SEQUENCES
+  ALTER SEQUENCE criteria_reports_id_seq RESTART;
+  
+success:=true;
+RETURN success;
+END
+$$ LANGUAGE plpgsql;
