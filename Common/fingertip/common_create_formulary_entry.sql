aa57f32b0c178f348df7ae118f0749b8f383f2c6
Ignacio <imonge@growthaccelerationpartners.com>
2015-07-19 13:02:08 -0600

    ANA-529 Update util methods, simplify interface

--- Common/fingertip/common_create_formulary_entry.sql
+++ Common/fingertip/common_create_formulary_entry.sql
@@ -1,4 +1,4 @@
-﻿CREATE OR REPLACE FUNCTION common_create_formulary_entry(formulary_id INTEGER,drug_id INTEGER, tier_id INTEGER, is_reset_mode BOOLEAN, reason_code_id INTEGER,specialty_copay BOOLEAN) --FF_NEW DB
+﻿CREATE OR REPLACE FUNCTION common_create_formulary_entry(formulary_id INTEGER,drug_id INTEGER, tier_id INTEGER, reason_code_id INTEGER,specialty_copay BOOLEAN) --FF_NEW DB
 RETURNS INTEGER AS $$
 DECLARE
 formulary_entry_id INTEGER DEFAULT NULL;
@@ -12,7 +12,7 @@ IF formulary_entry_id IS NULL THEN
 	INSERT INTO formularyentry(
         formularyfid, drugfid, tierfid, isresetmode, legacyfid, reasoncodefid, 
         updated_at, updated_by, specialty_copay, preferred, restriction_comments)
-        VALUES (formulary_id, drug_id, tier_id, CASE when is_reset_mode IS NULL THEN FALSE ELSE is_reset_mode END, NULL, reason_code_id, 
+        VALUES (formulary_id, drug_id, tier_id, FALSE, NULL, CASE when reason_code_id IS NULL THEN 0 ELSE reason_code_id END, 
         current_timestamp, NULL, CASE when specialty_copay IS NULL THEN FALSE ELSE specialty_copay END, NULL, NULL) RETURNING id INTO formulary_entry_id;
 
 	RETURN formulary_entry_id;
--- Common/fingertip/common_create_provider.sql
+++ Common/fingertip/common_create_provider.sql
@@ -1,10 +1,10 @@
-﻿CREATE OR REPLACE FUNCTION common_create_provider(is_active BOOLEAN, provider_name VARCHAR, web_name VARCHAR,suppress_roll_up INTEGER,alt_web_name VARCHAR, parents_id INTEGER) --FF_NEW DB
+﻿CREATE OR REPLACE FUNCTION common_create_provider(is_active BOOLEAN, provider_name VARCHAR, parents_id INTEGER) --FF_NEW DB
 RETURNS INTEGER AS $$
 DECLARE
 provider_id INTEGER DEFAULT NULL;
 BEGIN
 
-SELECT p.id INTO provider_id FROM provider p WHERE p.name=provider_name and p.webname=web_name and p.altwebname=alt_web_name  LIMIT 1;
+SELECT p.id INTO provider_id FROM provider p WHERE p.name=provider_name LIMIT 1;
 
 --VALIDATE IF THE PROVIDER ALREADY EXISTS
 IF provider_id IS NULL THEN
@@ -13,7 +13,7 @@ IF provider_id IS NULL THEN
 	INSERT INTO provider(isactive, name, webname, suppressrollup, altwebname, parentsfid, 
             corporatestructure, ptdates, ptmembers, ptcomments, top_provider, 
             is_medical_benefits)
-       VALUES (CASE when is_active IS NULL THEN TRUE ELSE is_active END, provider_name, web_name, CASE when suppress_roll_up IS NULL THEN 0 ELSE suppress_roll_up END, alt_web_name, parents_id, 
+       VALUES (CASE when is_active IS NULL THEN TRUE ELSE is_active END, provider_name, provider_name, 0, provider_name, parents_id, 
             NULL, NULL, NULL, NULL, FALSE, 
             FALSE) RETURNING id INTO provider_id;
 
