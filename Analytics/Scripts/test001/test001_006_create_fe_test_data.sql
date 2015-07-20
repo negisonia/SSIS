1bc0df575f2945df39bfff9761d2307a9496719f
Ignacio Monge <imonge@growthaccelerationpartners.com>
2015-07-20 10:08:20 -0600

    ANA-529 Fix fe test data script

--- Analytics/Scripts/test001/test001_006_create_fe_test_data.sql
+++ Analytics/Scripts/test001/test001_006_create_fe_test_data.sql
@@ -10,9 +10,9 @@ success BOOLEAN:=FALSE;
 BEGIN
 
 --GET the input data
-drug_001_id = SELECT id from drugs where name = 'DRUG_001' limit 1 INTO drug_001_id;
-health_plan_001_id = SELECT id from health_plan_types where name = 'HEALTH_PLAN_TYPE_001' limit 1 INTO health_plan_type_001_id;
-state_001_id = SELECT id from states where name = 'STATE_001' limit 1 INTO state_001_id;
+SELECT id from drugs where name = 'DRUG_001' limit 1 INTO drug_001_id;
+SELECT id from health_plan_types where name = 'HEALTH_PLAN_TYPE_001' limit 1 INTO health_plan_type_001_id;
+SELECT id from states where name = 'STATE_001' limit 1 INTO state_001_id;
 
 PERFORM create_criteria_report(0,0,0,0,2,FALSE,FALSE,FALSE,ARRAY[drug_001_id],ARRAY[health_plan_001_id],'State',ARRAY[state_001_id]);
 
