c8bb9e0caaee71e467000d501a56b34ddd9e2b3f
vsansilvestre <lema017@gmail.com>
2015-08-04 11:03:01 -0600

    adding changes to fix errors on test011 script

--- Common/data_entry/common_create_pior_authorization.sql
+++ Common/data_entry/common_create_pior_authorization.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE FUNCTION common_create_prior_authorization(new_data_entry_id INTEGER, active BOOLEAN) --DATA ENTRY
+CREATE OR REPLACE FUNCTION common_create_prior_authorization(new_data_entry_id INTEGER, new_active BOOLEAN) --DATA ENTRY
 RETURNS INTEGER AS $$
 DECLARE
 prior_authorization_id INTEGER DEFAULT NULL;
 BEGIN
 
-SELECT pai.id INTO prior_authorization_id FROM prior_authorizations pai WHERE pai.data_entry_id=new_data_entry_id AND c.is_active=active LIMIT 1;
+SELECT pai.id INTO prior_authorization_id FROM prior_authorizations pai WHERE pai.data_entry_id=new_data_entry_id AND pai.is_active=active LIMIT 1;
 
 --VALIDATE IF THE PRIOR AUTHORIZATION ALREADY EXISTS
 IF prior_authorization_id IS NULL THEN
@@ -14,8 +14,8 @@ IF prior_authorization_id IS NULL THEN
             updated_at, indication_id, provider_id, healthplantype_id, boolean_expression_tree, 
             duration_unit, active, is_active, copiedfromid, atomic_step_id)
     VALUES (NULL, NULL, NULL,new_data_entry_id, current_timestamp,
-            current_timestamp, NULL, NULL, NULL, FALSE,
-            NULL, active, active, NULL, NULL) RETURNING id INTO prior_authorization_id;
+            current_timestamp, NULL, NULL, NULL, NULL,
+            NULL, new_active, new_active, NULL, NULL) RETURNING id INTO prior_authorization_id;
   RETURN prior_authorization_id;
 ELSE
   RETURN prior_authorization_id;
---
+++ Common/data_entry/throw_error.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE FUNCTION throw_error(text)
+  RETURNS void AS $$
+		BEGIN
+			RAISE EXCEPTION '%', $1;
+		END;
+$$ LANGUAGE plpgsql;
