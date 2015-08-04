2989d504ef6005ee4ae4cc43de50487188ca3c5b
Ignacio <imonge@growthaccelerationpartners.com>
2015-08-04 17:45:36 -0600

    ANA-679 Added validation scripts

--- Analytics/Scripts/rpt_coverage_tier_geo/test_001/test_001_validate_test_data.sql
+++ Analytics/Scripts/rpt_coverage_tier_geo/test_001/test_001_validate_test_data.sql
@@ -2,28 +2,26 @@ CREATE OR REPLACE FUNCTION ana_rpt_coverage_tier_geo_test_001_validate_data() --
 RETURNS boolean AS $$
 DECLARE
   success boolean DEFAULT FALSE;
-  expected_value double precision := 350.50;
+  expected_value double precision;
   actual_value INTEGER;
   criteria_report_id INTEGER;
   current_month_int INTEGER;
 BEGIN
 
 -- Current Month
-SELECT extract(month from date_trunc('month', current_date)) INTO current_month_int;
+SELECT get_current_month() INTO current_month_int;
 -- Create Criteria Report Id
 SELECT ana_rpt_coverage_tier_geo_test_001_002_create_fe_data() INTO criteria_report_id;
 --Query the actual value
-SELECT total_lives from rpt_coverage_tier_geo(criteria_report_id,current_month_int) where market_name='STATE_001' limit 1 INTO actual_value;
-
-IF actual_value IS NULL OR actual_value != expected_value THEN
-  SELECT throw_error('ana_rpt_coverage_tier_geo_test_001_validate_data-error: EXPECTED VALUE OF ' || concat_ws(' GOT ', expected_value, actual_value));
-END IF;
-
 SELECT total_lives from rpt_coverage_tier_geo(criteria_report_id,current_month_int) where market_name='STATE_002' limit 1 INTO actual_value;
+expected_value = 335.75;
 
-IF actual_value IS NULL OR actual_value != expected_value THEN
-  SELECT throw_error('ana_rpt_coverage_tier_geo_test_001_validate_data-error: EXPECTED VALUE OF ' || concat_ws(' GOT ', expected_value, actual_value));
-END IF;
+PERFORM validate_comparison_values(actual_value, expected_value,'ana_rpt_coverage_tier_geo_test_001_validate_data-error: EXPECTED TOTAL LIVES FOR STATE_001 TO BE ');
+
+SELECT total_lives from rpt_coverage_tier_geo(criteria_report_id,current_month_int) where market_name='STATE_003' limit 1 INTO actual_value;
+expected_value = 200.5;
+
+PERFORM validate_comparison_values(actual_value, expected_value,'ana_rpt_coverage_tier_geo_test_001_validate_data-error: EXPECTED TOTAL LIVES FOR STATE_002 TO BE ');
 
 success:=true;
 RETURN success;
