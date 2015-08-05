174e98cd34b994242ab178e4343aed3f8fb94dae
Ignacio Monge <imonge@growthaccelerationpartners.com>
2015-08-05 15:37:03 -0600

    ANA-679 Validation scripts for test 009-016

--- Analytics/Scripts/rpt_coverage_tier_geo/common_front_end/test_015_016_create_fe_test_data.sql
+++ Analytics/Scripts/rpt_coverage_tier_geo/common_front_end/test_015_016_create_fe_test_data.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION ana_rpt_coverage_tier_geo_test_011_014_create_fe_data() --FF NEW DB
+CREATE OR REPLACE FUNCTION ana_rpt_coverage_tier_geo_test_015_016_create_fe_data() --FF NEW DB
 RETURNS INTEGER AS $$
 DECLARE
 
--- Analytics/Scripts/rpt_coverage_tier_geo/test_008/test_008_validate_test_data.sql
+++ Analytics/Scripts/rpt_coverage_tier_geo/test_008/test_008_validate_test_data.sql
@@ -16,7 +16,7 @@ SELECT ana_rpt_coverage_tier_geo_test_007_010_create_fe_data() INTO criteria_rep
 --Query the actual value
 SELECT calculate_report_value('SUM(lives)', get_report_name_call('rpt_coverage_tier_geo', ARRAY[criteria_report_id,current_month_int]),'dim_tier_name=''Tier 1'' OR dim_tier_name=''Tier 2''') INTO actual_value;
 --Compare actual and expected values
-PERFORM validate_comparison_values(actual_value, expected_value,'ana_rpt_coverage_tier_geo_test_008_validate_data-error: EXPECTED TOTAL LIVES FOR TIER 0101, 002 TO BE ');
+PERFORM validate_comparison_values(actual_value, expected_value,'ana_rpt_coverage_tier_geo_test_008_validate_data-error: EXPECTED TOTAL LIVES FOR TIER 001 TO BE ');
 
 success:=true;
 RETURN success;
