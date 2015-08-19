4a0236e8eed8bea90cf75e67435f7d1999524cc0
vsansilvestre <lema017@gmail.com>
2015-08-18 18:31:26 -0600

    updating utilitary functions to create restrictions and criterias with specific id

--- Common/data_entry/test_data_criteria.sql
+++ Common/data_entry/test_data_criteria.sql
@@ -5,20 +5,19 @@ success BOOLEAN:=FALSE;
 BEGIN
 
 --CREATE CRITERIAS
-PERFORM common_create_criteria('criteria_diagnosis_1', FALSE, TRUE);
-PERFORM common_create_criteria('criteria_diagnosis_2', FALSE, TRUE);
-PERFORM common_create_criteria('criteria_diagnosis_3', FALSE, TRUE);
-PERFORM common_create_criteria('criteria_unspecified', FALSE, TRUE);
-PERFORM common_create_criteria('criteria_exclusion_1', TRUE, TRUE);
-PERFORM common_create_criteria('criteria_clinical_1', FALSE, TRUE);
-PERFORM common_create_criteria('criteria_clinical_2', FALSE, TRUE);
-PERFORM common_create_criteria('criteria_clinical_3', FALSE, TRUE);
-PERFORM common_create_criteria('criteria_lab_1', FALSE, TRUE);
-PERFORM common_create_criteria('criteria_lab_2', TRUE, FALSE);
-PERFORM common_create_criteria('criteria_lab_3', FALSE, TRUE);
-PERFORM common_create_criteria('criteria_age_1', TRUE, TRUE);
-PERFORM common_create_criteria('criteria_ql_1', TRUE, TRUE);
-
+PERFORM common_create_criteria(1,'criteria_diagnosis_1', FALSE, TRUE);
+PERFORM common_create_criteria(2,'criteria_unspecified', FALSE, TRUE);
+PERFORM common_create_criteria(3,'criteria_diagnosis_2', FALSE, TRUE);
+PERFORM common_create_criteria(4,'criteria_diagnosis_3', FALSE, TRUE);
+PERFORM common_create_criteria(5,'criteria_exclusion_1', TRUE, TRUE);
+PERFORM common_create_criteria(6,'criteria_clinical_1', FALSE, TRUE);
+PERFORM common_create_criteria(7,'criteria_clinical_2', FALSE, TRUE);
+PERFORM common_create_criteria(8,'criteria_clinical_3', FALSE, TRUE);
+PERFORM common_create_criteria(9,'criteria_lab_1', FALSE, TRUE);
+PERFORM common_create_criteria(10,'criteria_lab_2', TRUE, FALSE);
+PERFORM common_create_criteria(11,'criteria_lab_3', FALSE, TRUE);
+PERFORM common_create_criteria(12,'criteria_age_1', TRUE, TRUE);
+PERFORM common_create_criteria(13,'criteria_ql_1', TRUE, TRUE);
 
 success=true;
 return success;
--- Common/data_entry/test_data_restrictions.sql
+++ Common/data_entry/test_data_restrictions.sql
@@ -5,21 +5,30 @@ success BOOLEAN:=FALSE;
 BEGIN
 
 --CREATE CRITERIAS
-PERFORM common_create_restriction('Diagnosis','PA');
-PERFORM common_create_restriction('Diagnosis','Medical');
-PERFORM common_create_restriction('Unspecified','PA');
-PERFORM common_create_restriction('Unspecified','Medical');
-PERFORM common_create_restriction('Exclusion','PA');
-PERFORM common_create_restriction('Exclusion','Medical');
-PERFORM common_create_restriction('Clinical','PA');
-PERFORM common_create_restriction('Clinical','Medical');
-PERFORM common_create_restriction('Labs','PA');
-PERFORM common_create_restriction('Labs','Medical');
-PERFORM common_create_restriction('Age','PA');
-PERFORM common_create_restriction('Age','Medical');
-PERFORM common_create_restriction('QL','QL');
-PERFORM common_create_restriction('PA','PA');
-PERFORM common_create_restriction('Medical','Medical');
+PERFORM common_create_restriction(1,'PA','PA');
+PERFORM common_create_restriction(2,'ST','ST');
+PERFORM common_create_restriction(3,'Medical','Medical');
+PERFORM common_create_restriction(4,'Unspecified','Medical');
+PERFORM common_create_restriction(5,'OR','OR');
+PERFORM common_create_restriction(6,'Step','PA');
+PERFORM common_create_restriction(7,'Diagnosis','PA');
+PERFORM common_create_restriction(8,'Exclusion','PA');
+PERFORM common_create_restriction(9,'Labs','PA');
+PERFORM common_create_restriction(10,'MD','PA');
+PERFORM common_create_restriction(11,'Step','Medical');
+PERFORM common_create_restriction(12,'Diagnosis','Medical');
+PERFORM common_create_restriction(13,'Exclusion','Medical');
+PERFORM common_create_restriction(14,'Labs','Medical');
+PERFORM common_create_restriction(15,'MD','Medical');
+PERFORM common_create_restriction(16,'Unspecified','PA');
+PERFORM common_create_restriction(17,'Unspecified','Medical');
+PERFORM common_create_restriction(18,'Clinical','PA');
+PERFORM common_create_restriction(19,'Clinical','Medical');
+PERFORM common_create_restriction(20,'Age','PA');
+PERFORM common_create_restriction(21,'Age','Medical');
+PERFORM common_create_restriction(22,'Non-Preferred','PA');
+PERFORM common_create_restriction(23,'Non-Preferred','Medical');
+PERFORM common_create_restriction(24,'QL','QL');
 
 success=true;
 return success;
