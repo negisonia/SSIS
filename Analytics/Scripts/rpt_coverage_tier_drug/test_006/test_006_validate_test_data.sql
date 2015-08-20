faee8c27b421643aa1948c98629c2a10776a9953
Ignacio Monge <imonge@growthaccelerationpartners.com>
2015-08-20 10:42:02 -0600

    ANA740 Fix broken test due to new data inserted

--- Analytics/Scripts/rpt_coverage_tier_drug/test_006/test_006_validate_test_data.sql
+++ Analytics/Scripts/rpt_coverage_tier_drug/test_006/test_006_validate_test_data.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION ana_rpt_coverage_tier_drug_test_006_validate_data() -
 RETURNS boolean AS $$
 DECLARE
   success boolean DEFAULT FALSE;
-  expected_value INTEGER := 2;
+  expected_value INTEGER := 3;
   actual_value INTEGER;
   criteria_report_id INTEGER;
   current_month_int INTEGER;
@@ -15,17 +15,12 @@ BEGIN
 SELECT extract(month from date_trunc('month', current_date)) INTO current_month_int;
 -- Create Criteria Report Id
 SELECT ana_rpt_coverage_tier_drug_test_001_007_create_fe_data() INTO criteria_report_id;
--- Get Drug 001 id
-SELECT id from drugs where name = 'DRUG_002' limit 1 INTO drug_002_id;
--- Get Tier id
-SELECT id from dim_tier where name = 'Tier 1' limit 1 INTO tier_001_id;
-SELECT id from dim_tier where name = 'Tier 2' limit 1 INTO tier_002_id;
---Query the actual value
-SELECT SUM(health_plan_count) from rpt_coverage_tier_drug(criteria_report_id,current_month_int) where drug_id = drug_002_id and (dim_tier_id = tier_001_id or dim_tier_id = tier_002_id) AND is_tier_preferred IS FALSE INTO actual_value;
 
+--Query the actual value
+SELECT calculate_report_value('SUM(health_plan_count)', get_report_name_call('rpt_coverage_tier_drug', ARRAY[criteria_report_id,current_month_int]),'(dim_tier_name=''Tier 1'' OR dim_tier_name=''Tier 2'') AND drug_name=''DRUG_002'' AND is_tier_preferred IS FALSE') INTO actual_value;
 
 IF actual_value IS NULL OR actual_value != expected_value THEN
-  SELECT throw_error('test_006_validate_test_data-error: EXPECTED VALUE OF ' || concat_ws('GOT ', expected_value, actual_value));
+  SELECT throw_error('test_006_validate_test_data-error: EXPECTED VALUE OF ' || concat_ws(' GOT ', expected_value, actual_value));
 END IF; 
 
 success:=true;
