612ad095c28e3d50ca27fa77c851f1b27e88252f
Vicente San Silvestre <vsansilvestre@growthaccelerationparters.com>
2015-09-02 10:34:39 -0600

    adding changes to add notes

--- Common/data_entry/common_create_prior_authorization_criteria.sql
+++ Common/data_entry/common_create_prior_authorization_criteria.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION common_create_prior_authorization_criteria(new_prior_authorization_id INTEGER, new_criteria_id INTEGER, new_active BOOLEAN, new_criterium_applicable INTEGER, new_value_lower INTEGER, new_value_upper INTEGER) --DATA ENTRY
+CREATE OR REPLACE FUNCTION common_create_prior_authorization_criteria(new_prior_authorization_id INTEGER, new_criteria_id INTEGER, new_active BOOLEAN, new_criterium_applicable INTEGER, new_value_lower INTEGER, new_value_upper INTEGER, new_notes VARCHAR) --DATA ENTRY
 RETURNS BOOLEAN AS $$
 DECLARE
 success BOOLEAN DEFAULT FALSE;
@@ -9,14 +9,14 @@ SELECT EXISTS (SELECT 1 FROM prior_authorization_criteria pac WHERE pac.is_activ
 
 --VALIDATE IF THE PRIOR AUTHORIZATION CRITERIA ALREADY EXISTS
 IF valueExists IS FALSE THEN
-  --INSERT PRIOR AUTHORIZATION CRITERIA RECORD
-INSERT INTO prior_authorization_criteria(
+--INSERT PRIOR AUTHORIZATION CRITERIA RECORD
+INSERT INTO public.prior_authorization_criteria(
             value_upper, value_lower, non_fda_approved, created_at, updated_at,
             prior_authorization_id, criterium_id, criterium_applicable, active,
-            is_active, copiedfromid)
-    VALUES (new_value_upper, new_value_lower, NULL, current_timestamp, current_timestamp,
-            new_prior_authorization_id, new_criteria_id, new_criterium_applicable, new_active,new_active,
-            NULL);
+            is_active, copiedfromid, notes)
+VALUES (new_value_upper, new_value_lower, NULL, current_timestamp, current_timestamp,
+        new_prior_authorization_id, new_criteria_id, new_criterium_applicable, new_active,
+        new_active, NULL, new_notes);
 END IF;
 
 success :=TRUE;
--- Common/data_entry/common_create_quantity_limit_criteria.sql
+++ Common/data_entry/common_create_quantity_limit_criteria.sql
@@ -1,17 +1,18 @@
-CREATE OR REPLACE FUNCTION common_create_quantity_limit_criteria(new_quantity_limit_id INTEGER, new_criteria_id INTEGER, active BOOLEAN, new_amount_val INTEGER, new_time_val INTEGER, new_amount_type VARCHAR,  new_time_type VARCHAR) --DATA ENTRY
+CREATE OR REPLACE FUNCTION common_create_quantity_limit_criteria(new_quantity_limit_id INTEGER, new_criteria_id INTEGER, new_active BOOLEAN, new_amount_val INTEGER, new_time_val INTEGER, new_amount_type VARCHAR,  new_time_type VARCHAR,new_notes VARCHAR) --DATA ENTRY
 RETURNS BOOLEAN AS $$
 DECLARE
 success BOOLEAN DEFAULT FALSE;
 BEGIN
 
   --INSERT QUANTITY LIMIT CRITERIA RECORD
-INSERT INTO quantity_limit_criteria(
-             created_at, updated_at, criterium_id, quantity_limit_id,
+INSERT INTO public.quantity_limit_criteria(
+            created_at, updated_at, criterium_id, quantity_limit_id,
             active, amount_val, time_val, amount_type, time_type, is_active,
-            copiedfromid)
-    VALUES (current_timestamp, current_timestamp, new_criteria_id, new_quantity_limit_id,
-            active,new_amount_val, new_time_val, new_amount_type, new_time_type,active,
-            null);
+            copiedfromid, notes)
+    VALUES ( current_timestamp, current_timestamp, new_criteria_id, new_quantity_limit_id,
+            new_active, new_amount_val, new_time_val, new_amount_type, new_time_type, new_active,
+            NULL, new_notes);
+
 
 success :=TRUE;
 return success;
