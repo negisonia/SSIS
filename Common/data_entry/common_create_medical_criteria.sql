9d73723b3b3ffd73c2660400efa6c7fd5515c4ba
Vicente San Silvestre <vsansilvestre@growthaccelerationparters.com>
2015-09-02 12:28:05 -0600

    adding changes to add notes

--- Common/data_entry/common_create_medical_criteria.sql
+++ Common/data_entry/common_create_medical_criteria.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION common_create_medical_criteria(new_medical_id INTEGER, new_criteria_id INTEGER, active BOOLEAN) --DATA ENTRY
+CREATE OR REPLACE FUNCTION common_create_medical_criteria(new_medical_id INTEGER, new_criteria_id INTEGER, new_active BOOLEAN, new_notes VARCHAR) --DATA ENTRY
 RETURNS BOOLEAN AS $$
 DECLARE
 success BOOLEAN DEFAULT FALSE;
@@ -10,13 +10,13 @@ SELECT EXISTS (SELECT 1 FROM medical_criteria mc WHERE mc.medical_id=new_medical
 --VALIDATE IF THE MEDICAL CRITERIA ALREADY EXISTS
 IF valueExists IS FALSE THEN
   --INSERT MEDICAL CRITERIA CRITERIA RECORD
-INSERT INTO medical_criteria(
-            value_upper, value_lower, non_fda_approved, created_at, updated_at,
-            medical_id, criterium_id, criterium_applicable, active, is_active,
-            copiedfromid)
-    VALUES (NULL, NULL, NULL, current_timestamp, current_timestamp,
-            new_medical_id, new_criteria_id, 2, active,active,
-            NULL);
+INSERT INTO public.medical_criteria(
+        value_upper, value_lower, non_fda_approved, created_at, updated_at,
+        medical_id, criterium_id, criterium_applicable, active, is_active,
+        copiedfromid, notes)
+VALUES (NULL, NULL, NULL, current_timestamp, current_timestamp,
+        new_medical_id, new_criteria_id, 2, new_active,new_active,
+        NULL,new_notes);
 END IF;
 
 success :=TRUE;
--- Common/data_entry/test_data_medicals.sql
+++ Common/data_entry/test_data_medicals.sql
@@ -96,7 +96,7 @@ SELECT hpt.id INTO hix_health_plan_type FROM ff.health_plan_types_import hpt WHE
     --INSERT MEDICAL
     SELECT  common_create_medical(data_entry_id,TRUE,atomic_step_id) INTO medical_id;
     --INSERT MEDICAL CRITERIA
-    PERFORM common_create_medical_criteria(medical_id, criteria_diagnosis_3, TRUE);
+    PERFORM common_create_medical_criteria(medical_id, criteria_diagnosis_3, TRUE,NULL);
     PERFORM common_update_data_entry(data_entry_id, NULL, NULL, NULL, NULL, medical_id);
 
     --INSERT DATA ENTRY
@@ -104,7 +104,7 @@ SELECT hpt.id INTO hix_health_plan_type FROM ff.health_plan_types_import hpt WHE
     --INSERT MEDICAL
     SELECT common_create_medical(data_entry_id,TRUE,NULL) INTO medical_id;
     --INSERT MEDICAL CRITERIA
-    PERFORM common_create_medical_criteria(medical_id, criteria_unspecified, TRUE);
+    PERFORM common_create_medical_criteria(medical_id, criteria_unspecified, TRUE,'additional notes');
     PERFORM common_update_data_entry(data_entry_id, NULL, NULL, NULL, NULL, medical_id);
 
      --INSERT DATA ENTRY
@@ -112,7 +112,7 @@ SELECT hpt.id INTO hix_health_plan_type FROM ff.health_plan_types_import hpt WHE
     --INSERT MEDICAL
     SELECT common_create_medical(data_entry_id,TRUE,NULL) INTO medical_id;
     --INSERT MEDICAL CRITERIA
-    PERFORM common_create_medical_criteria(medical_id, criteria_age_1, TRUE);
+    PERFORM common_create_medical_criteria(medical_id, criteria_age_1, TRUE,'additional notes');
     PERFORM common_update_data_entry(data_entry_id, NULL, NULL, NULL, NULL, medical_id);
 
      --INSERT DATA ENTRY
@@ -120,7 +120,7 @@ SELECT hpt.id INTO hix_health_plan_type FROM ff.health_plan_types_import hpt WHE
     --INSERT MEDICAL
     SELECT common_create_medical(data_entry_id,TRUE,NULL) INTO medical_id;
     --INSERT MEDICAL CRITERIA
-    PERFORM common_create_medical_criteria(medical_id, criteria_age_1, TRUE);
+    PERFORM common_create_medical_criteria(medical_id, criteria_age_1, TRUE, NULL);
     PERFORM common_update_data_entry(data_entry_id, NULL, NULL, NULL, NULL, medical_id);
 
 success=true;
