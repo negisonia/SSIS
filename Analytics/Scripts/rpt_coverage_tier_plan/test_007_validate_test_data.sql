82655711c74e85d886fe683de1c803d1d474d06a
Ignacio <imonge@growthaccelerationpartners.com>
2015-09-03 17:27:31 -0600

    ANA-743 Add qualifiers

--- Analytics/Scripts/rpt_coverage_tier_plan/test_007_validate_test_data.sql
+++ Analytics/Scripts/rpt_coverage_tier_plan/test_007_validate_test_data.sql
@@ -7,16 +7,17 @@ DECLARE
 BEGIN
 
 -- TEST PLAN 008
-PERFORM ana_rpt_coverage_tier_plan_test007_validate_has_restriction_set(false,false,false,false,'TEST_PLAN_008');
+-- QL, PA, ST, OR
+PERFORM ana_rpt_coverage_tier_plan_test007_validate_has_restriction_set(false,true,true,false,'TEST_PLAN_008');
 
 -- TEST PLAN 009
-PERFORM ana_rpt_coverage_tier_plan_test007_validate_has_restriction_set(false,false,false,false,'TEST_PLAN_009');
+PERFORM ana_rpt_coverage_tier_plan_test007_validate_has_restriction_set(true,false,true,false,'TEST_PLAN_009');
 
 -- TEST PLAN 010
-PERFORM ana_rpt_coverage_tier_plan_test007_validate_has_restriction_set(false,false,false,false,'TEST_PLAN_010');
+PERFORM ana_rpt_coverage_tier_plan_test007_validate_has_restriction_set(true,false,false,false,'TEST_PLAN_010');
 
 -- TEST PLAN 012
-PERFORM ana_rpt_coverage_tier_plan_test007_validate_has_restriction_set(false,false,false,false,'TEST_PLAN_012');
+PERFORM ana_rpt_coverage_tier_plan_test007_validate_has_restriction_set(true,true,true,true,'TEST_PLAN_012');
 
 health_plan_names := ARRAY['TEST_PLAN_013','TEST_PLAN_017','TEST_PLAN_018','TEST_PLAN_019'];
 FOREACH plan_name IN ARRAY health_plan_names
