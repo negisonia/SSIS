5cc3638d66c55697b37edd54c595acecf8564c76
Vicente San Silvestre <vsansilvestre@growthaccelerationparters.com>
2015-09-07 15:23:19 -0600

    adding changes for test 018

--- Load/load_front_end.sql
+++ Load/load_front_end.sql
@@ -17,7 +17,8 @@
 \i 'Common/front_end/calculate_report_value_boolean.sql'
 \i 'Common/front_end/validate_comparison_values_boolean.sql'
 \i 'Common/front_end/validate_comparison_values_varchar.sql'
-\i 'Common/front_end/validate_summary_table.sql'
+\i 'Common/front_end/res_rpt_summary_table_validate_data.sql'
+\i 'Common/front_end/res_rpt_drug_validate_data.sql'
 \i 'Common/front_end/common_get_dim_criteria.sql'
 
 
