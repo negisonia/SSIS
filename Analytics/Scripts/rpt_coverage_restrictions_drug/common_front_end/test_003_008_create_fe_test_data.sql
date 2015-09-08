6b3cfe7d4a4c62361c4be28d6e3779343c906f63
Ignacio <imonge@growthaccelerationpartners.com>
2015-09-08 13:34:46 -0600

    ANA747 Create fe test data scripts for summary state level report

---
+++ Analytics/Scripts/rpt_coverage_restrictions_drug/common_front_end/test_003_008_create_fe_test_data.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION ana_rpt_coverage_restrictions_drug_test_003_008_create_fe_data()
+RETURNS INTEGER AS $$
+DECLARE
+
+criteria_report_id INTEGER;
+
+BEGIN
+
+SELECT ana_rpt_create_criteria_report_fe_data(ARRAY['STATE_001'],ARRAY['HEALTH_PLAN_TYPE_001','HEALTH_PLAN_TYPE_002'],ARRAY['DRUG_002'],'State') INTO criteria_report_id;
+
+RETURN criteria_report_id;
+END
+$$ LANGUAGE plpgsql;
