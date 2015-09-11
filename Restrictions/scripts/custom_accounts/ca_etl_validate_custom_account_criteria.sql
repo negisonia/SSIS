cfbe3ef781a1130a07ab9a45d13149907795c7f4
Ignacio <imonge@growthaccelerationpartners.com>
2015-09-11 12:01:23 -0600

    fx_ca_etl_3 Changes to custom accounts, point to health plan level only

--- Restrictions/scripts/custom_accounts/ca_etl_validate_custom_account_criteria.sql
+++ Restrictions/scripts/custom_accounts/ca_etl_validate_custom_account_criteria.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION ca_etl_validate_custom_account_criteria(ca_id integer, provider_names varchar[], health_plan_names varchar[], health_plan_table_name varchar, provider_table_name varchar) --Data Warehouse
+CREATE OR REPLACE FUNCTION ca_etl_validate_custom_account_criteria(ca_id integer, provider_names varchar[], health_plan_names varchar[], health_plan_table_name varchar, provider_table_name varchar, plan_type_level boolean) --Data Warehouse
 RETURNS boolean AS $$
 DECLARE
   success boolean DEFAULT FALSE;
@@ -26,17 +26,25 @@ BEGIN
     LOOP
       SELECT common_get_table_id_by_name(health_plan_table_name, plan_name) INTO plan_id;
 
-      SELECT EXISTS (SELECT 1 FROM admin.custom_account_health_plans cahp WHERE cahp.custom_account_id = ca_id AND cahp.health_plan_id = plan_id) INTO booleanValue;
-      IF booleanValue IS FALSE THEN
-        select throw_error('ca_etl_test-error: EXPECTED Plan ' || plan_name || ' to be associated with CUSTOM ACCOUNT ' || ca_id);
+      IF plan_type_level IS FALSE THEN
+
+        SELECT EXISTS (SELECT 1 FROM admin.custom_account_health_plans cahp WHERE cahp.custom_account_id = ca_id AND cahp.health_plan_id = plan_id) INTO booleanValue;
+        IF booleanValue IS FALSE THEN
+          select throw_error('ca_etl_test-error: EXPECTED Plan ' || plan_name || ' to be associated with CUSTOM ACCOUNT ' || ca_id);
+        END IF;
+        
       END IF;
 
-      EXECUTE 'SELECT provider_id from ' || health_plan_table_name || ' where id = ''' || plan_id || ''' LIMIT 1' INTO prov_id;
-      EXECUTE 'SELECT health_plan_type_id from ' || health_plan_table_name || ' where id = ''' || plan_id || ''' LIMIT 1' INTO plan_type_id;
+      IF plan_type_level IS TRUE THEN
 
-      SELECT EXISTS (SELECT 1 FROM admin.custom_account_provider_plant_types cappt WHERE cappt.custom_account_id = ca_id AND cappt.health_plan_type_id = plan_type_id AND cappt.provider_id = prov_id) INTO booleanValue;
-      IF booleanValue IS FALSE THEN
-        select throw_error('ca_etl_test-error: WRONG PLAN TYPE AND PROVIDER ASSOCIATION FOR CUSTOM ACCOUNT ' || ca_id || ' PLAN TYPE ' || plan_type_id ||' PROVIDER ' || prov_id);
+        EXECUTE 'SELECT provider_id from ' || health_plan_table_name || ' where id = ''' || plan_id || ''' LIMIT 1' INTO prov_id;
+        EXECUTE 'SELECT health_plan_type_id from ' || health_plan_table_name || ' where id = ''' || plan_id || ''' LIMIT 1' INTO plan_type_id;
+
+        SELECT EXISTS (SELECT 1 FROM admin.custom_account_provider_plant_types cappt WHERE cappt.custom_account_id = ca_id AND cappt.health_plan_type_id = plan_type_id AND cappt.provider_id = prov_id) INTO booleanValue;
+        IF booleanValue IS FALSE THEN
+          select throw_error('ca_etl_test-error: WRONG PLAN TYPE AND PROVIDER ASSOCIATION FOR CUSTOM ACCOUNT ' || ca_id || ' PLAN TYPE ' || plan_type_id ||' PROVIDER ' || prov_id);
+        END IF;
+
       END IF;
 
     END LOOP;
