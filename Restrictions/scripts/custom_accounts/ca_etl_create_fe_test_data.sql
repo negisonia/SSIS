806da384446b507a7ec58f1c08c5ea092a9d4c0e
Ignacio Monge <imonge@growthaccelerationpartners.com>
2015-09-14 17:02:39 -0600

    ca_etl_test_3_fix Fix for etl test 3

--- Restrictions/scripts/custom_accounts/ca_etl_create_fe_test_data.sql
+++ Restrictions/scripts/custom_accounts/ca_etl_create_fe_test_data.sql
@@ -23,7 +23,7 @@ SELECT report_id from criteria_restriction_reports where report_name = 'report_1
 SELECT common_get_table_id_by_name('clients', 'client_2') INTO existing_client_id;
 SELECT custom_account_id from custom_accounts where name = 'Custom_Account_2' and client_id = existing_client_id INTO existing_custom_account_id;
 
-SELECT create_criteria_report(new_report_id,0,NULL,0,4,FALSE,FALSE,FALSE,ARRAY[drug_1_id,drug_2_id],ARRAY[]::integer[],'National',ARRAY[]::integer[],existing_custom_account_id,'national',NULL,NULL,NULL,NULL) INTO criteria_report_id;
+SELECT create_criteria_report(new_report_id,0,1,0,4,FALSE,FALSE,FALSE,ARRAY[drug_1_id,drug_2_id],ARRAY[]::integer[],'National',ARRAY[]::integer[],existing_custom_account_id,'national',NULL,NULL,NULL,NULL) INTO criteria_report_id;
 
 RETURN criteria_report_id;
 END
