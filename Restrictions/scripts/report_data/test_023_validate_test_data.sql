f3a27ecfe562b1b97c803f21bb923c3f04dacb49
Vicente San Silvestre <vsansilvestre@growthaccelerationparters.com>
2015-09-23 14:59:22 -0600

    compare string change

--- Restrictions/scripts/report_data/test_023_validate_test_data.sql
+++ Restrictions/scripts/report_data/test_023_validate_test_data.sql
@@ -29,17 +29,17 @@ SELECT common_get_table_id_by_name('health_plan_types','hix') INTO hix_hpt;
 --RETRIVE REPORT ID
 SELECT  crr.report_id INTO admin_report_1 FROM criteria_restriction_reports crr WHERE crr.report_name='report_1';
 
---VALIDATE NOTES (DRUG 2, QL, hix)
+--VALIDATE NOTES (DRUG 2, QL, commercial)
 expected_health_plan_notes_output= '['||
-    '{"indication_name":"Ind1","dim_criterion_type_id":2,"criterion_name":"criteria_ql_1","note_position":1,"notes":"1 tabs per 10 days\n\nql message"}'||
+    '{"indication_name":"Ind1","dim_criterion_type_id":1,"criterion_name":"criteria_ql_1","note_position":1,"notes":"1 tabs per 10 days\\n\\nql message"}'||
     ']';
-PERFORM rpt_health_plan_notes_validate_data(admin_report_1, provider_1, hix_hpt, drug_2, ql_dim_criterion_type, expected_health_plan_notes_output);
+PERFORM rpt_health_plan_notes_validate_data(admin_report_1, provider_1, commercial_hpt, drug_2, ql_dim_criterion_type, expected_health_plan_notes_output);
 
 --VALIDATE NOTES (DRUG 1, QL, commercial)
 expected_health_plan_notes_output= '['||
-    '{"indication_name":"Ind1","dim_criterion_type_id":2,"criterion_name":"criteria_ql_1","note_position":1,"notes":"2 tabs per 10 week"}'||
+    '{"indication_name":"Ind1","dim_criterion_type_id":1,"criterion_name":"criteria_ql_1","note_position":1,"notes":"2 tabs per 10 week"}'||
     ']';
-PERFORM rpt_health_plan_notes_validate_data(admin_report_1, provider_1, hix_hpt, drug_2, ql_dim_criterion_type, expected_health_plan_notes_output);
+PERFORM rpt_health_plan_notes_validate_data(admin_report_1, provider_1, commercial_hpt, drug_1, ql_dim_criterion_type, expected_health_plan_notes_output);
 
 success:=true;
 return success;
