87104731829314e688167176db2da2f3a6a84d6e
Ignacio <imonge@growthaccelerationpartners.com>
2015-09-29 16:16:45 -0600

    use distinct

--- Common/report_data/res_rpt_drug_validate_data.sql
+++ Common/report_data/res_rpt_drug_validate_data.sql
@@ -12,7 +12,7 @@ END IF;
 --VALIDATE RPT DRUG
 SELECT  array_to_json(array_agg(row_to_json(t))) from (select criteria_report_id, indication_name, drug_name, benefit_name, criteria_restriction_name, restriction_name, dim_restriction_type_id, lives, total_pharmacy_lives, health_plan_count, total_health_plan_count, total_medical_lives, provider_count, total_provider_count from rpt_drug(report_id, rpt_type) order by indication_name, drug_name, criteria_restriction_name) t INTO rpt_drug_output;
 
-IF rpt_drug_output!=expected_json THEN
+IF rpt_drug_output IS DISTINCT FROM expected_json THEN
 RAISE NOTICE 'ACTUAL: %s' , rpt_drug_output;
 RAISE NOTICE 'EXPECTED: %s', expected_json;
 SELECT throw_error('res_rpt_drug_validate_data : REPORT  DRUG  OUTPUT  MISMATCH');
