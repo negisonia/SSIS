500e4f3e74a8a2c4de9bdc6877598c65da5471c1
ignacio753 <ignacio@its-Mac-mini.local>
2015-10-01 16:12:07 -0600

    Add test 12

--- Restrictions/scripts/report_data/custom_accounts/ca_etl_test_12.sql
+++ Restrictions/scripts/report_data/custom_accounts/ca_etl_test_12.sql
@@ -3,7 +3,8 @@ RETURNS boolean AS $$
 DECLARE
 
   success BOOLEAN DEFAULT FALSE;
-  fe_report_1 INTEGER;
+  criteria_report_id INTEGER;
+
   expected_provider_notes VARCHAR;
   provider_1 INTEGER;
   commercial_hpt INTEGER;
@@ -50,46 +51,46 @@ BEGIN
   SELECT res_ca_etl_test_create_report_1_criteria_report_data() INTO criteria_report_id;
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":1,"criterion_name":"criteria_diagnosis_1","note_position":1,"notes":""}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, commercial_hpt, drug_1, ind1_pa_diagnosis_1, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, commercial_hpt, drug_1, ind1_pa_diagnosis_1, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":1,"criterion_name":"criteria_age_1","note_position":1,"notes":"age restriction"}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, commercial_hpt, drug_1, ind1_pa_age_1, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, commercial_hpt, drug_1, ind1_pa_age_1, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":1,"criterion_name":"criteria_diagnosis_3","note_position":1,"notes":"long message 100 characters"}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, commercial_hpt, drug_1, ind1_pa_diagnosis_3, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, commercial_hpt, drug_1, ind1_pa_diagnosis_3, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":1,"criterion_name":"criteria_clinical_1","note_position":1,"notes":"long message 500 characters"}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, commercial_hpt, drug_1, ind1_pa_clinical_1, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, commercial_hpt, drug_1, ind1_pa_clinical_1, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":2,"criterion_name":"PA/ST - Single - custom_option_1^1","note_position":1,"notes":"Drug1 notes: long message 500 characters"}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, commercial_hpt, drug_1, ind1_pa_past_custom_option_1, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, commercial_hpt, drug_1, ind1_pa_past_custom_option_1, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":1,"criterion_name":"Criteria Unspecified","note_position":1,"notes":"Notes"}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, hix_hpt, drug_1, ind1_pa_unspecified, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, hix_hpt, drug_1, ind1_pa_unspecified, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":1,"criterion_name":"criteria_diagnosis_1","note_position":1,"notes":"message"}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, hix_hpt, drug_2, ind1_pa_diagnosis_1, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, hix_hpt, drug_2, ind1_pa_diagnosis_1, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":1,"criterion_name":"criteria_age_1","note_position":1,"notes":""}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, commercial_hpt, drug_2, ind1_m_age_1, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, commercial_hpt, drug_2, ind1_m_age_1, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":1,"criterion_name":"Criteria Unspecified","note_position":1,"notes":"additional notes"}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, commercial_hpt, drug_2, ind1_m_unspecified, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, commercial_hpt, drug_2, ind1_m_unspecified, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":1,"criterion_name":"criteria_diagnosis_3","note_position":1,"notes":"notes"}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, hix_hpt, drug_2, ind1_m_criteria_diagnosis_3, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, hix_hpt, drug_2, ind1_m_criteria_diagnosis_3, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":2,"criterion_name":"ST - Single - custom_option_2^1 ","note_position":1,"notes":""}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, hix_hpt, drug_2, ind1_m_st_custom_option_2, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, hix_hpt, drug_2, ind1_m_st_custom_option_2, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":2,"criterion_name":"ST - Double - custom_option_1^1 AND custom_option_2^2","note_position":1,"notes":"Drug1 notes: notes for drug 1"},{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":2,"criterion_name":"ST - Double - custom_option_1^1 AND custom_option_2^2","note_position":2,"notes":"Drug2 notes: notes for drug 1"}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, hix_hpt, drug_2, ind1_pa_st_double_co_1_co_2, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, hix_hpt, drug_2, ind1_pa_st_double_co_1_co_2, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":1,"criterion_name":"criteria_ql_1","note_position":1,"notes":"1 tabs per 10 days\\n\\nql message"}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, commercial_hpt, drug_2, ind1_pa_ql, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, commercial_hpt, drug_2, ind1_pa_ql, expected_provider_notes);
 
   expected_provider_notes = '[{"indication_name":"indication_1","indication_abbre":"Ind1","dim_criterion_type_id":1,"criterion_name":"criteria_ql_1","note_position":1,"notes":"2 tabs per 10 week"}]';
-  PERFORM res_rpt_provider_notes_validate_data(fe_report_1, provider_1, commercial_hpt, drug_1, ind1_pa_ql, expected_provider_notes);
+  PERFORM res_rpt_provider_notes_validate_data(criteria_report_id, provider_1, commercial_hpt, drug_1, ind1_pa_ql, expected_provider_notes);
 
 success:=true;
 RETURN success;
