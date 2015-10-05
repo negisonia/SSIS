96858de306987f66acd6ab578136465f9097571d
vsansilvestregap <vsansilvestre@wearegap.com>
2015-10-05 16:46:17 -0600

    adding test 28 utilitary functions

--- Restrictions/scripts/report_data/test_028_validate_test_data.sql
+++ Restrictions/scripts/report_data/test_028_validate_test_data.sql
@@ -10,6 +10,7 @@ hix_plan_type_id INTEGER;
 drug_1 INTEGER;
 drug_2 INTEGER;
 druglist VARCHAR;
+
 BEGIN
 
 --RETRIEVE DATA
@@ -30,6 +31,11 @@ expected_source_comments_output = '['||
     '{"drug_id":2,"drug_name":"drug_2","pa_policy_form_url":"https://www.provider_1.com/providerLevelPAform.pdf"}'||
     ']';
 PERFORM res_rpt_pa_policy_form_url(fe_report_1, commercial_plan_type_id, provider_id, format(druglist,drug_1,drug_2), expected_source_comments_output);
+
+druglist := select array_to_string(array_agg(q.drug_id), ', ')  from (select distinct drug_id from specialty_pharmacy_drugs spd INNER JOIN (select hp.id as hp_id from health_plans hp inner join providers p on hp.provider_id=p.id inner join health_plan_types hpt on hp.health_plan_type_id=hpt.id
+ and p.id=1 and (hpt.id=1 or hpt.id=13)) q on spd.health_plan_id=q.hp_id) q;
+
+
 
 success:=true;
 return success;
