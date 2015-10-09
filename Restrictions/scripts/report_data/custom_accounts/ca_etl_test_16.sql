f8e033f7390c124229680c42d3c84487e844b8a7
Ignacio <imonge@growthaccelerationpartners.com>
2015-10-09 15:02:09 -0600

    ca_etl_test_16 Added test 16 and 18 of ca

--- Restrictions/scripts/report_data/custom_accounts/ca_etl_test_16.sql
+++ Restrictions/scripts/report_data/custom_accounts/ca_etl_test_16.sql
@@ -1,25 +1,25 @@
-CREATE OR REPLACE FUNCTION restrictions_test_030_validate_test_data() --REPORT FRONT END
+CREATE OR REPLACE FUNCTION res_ca_etl_test_16_validate_test_data() --FRONT END
 RETURNS boolean AS $$
 DECLARE
-success BOOLEAN DEFAULT FALSE;
-expected_rpt_geo_output VARCHAR;
-fe_report_1 INTEGER;
-drug_1 INTEGER;
-drug_2 INTEGER;
-pharmacy_benefit_type INTEGER := 1;
-state_market_type INTEGER := 2;
+  success boolean DEFAULT FALSE;
+  expected_output varchar;
+  report_id INTEGER;
+  drugs VARCHAR:='drugs';
+  drug_1 INTEGER;
+  drug_2 INTEGER;
+  pharmacy_benefit_type INTEGER := 1;
+  state_market_type INTEGER := 2;  
 BEGIN
 
---RETRIEVE DATA
+-- Create Report Id
+SELECT res_ca_etl_test_create_report_1_criteria_report_data() INTO report_id;
 
-SELECT common_get_table_id_by_name('drugs','drug_1') INTO drug_1;
-SELECT common_get_table_id_by_name('drugs','drug_2') INTO drug_2;
+-- Get parameter values
+SELECT common_get_table_id_by_name(drugs,'drug_1') INTO drug_1;
+SELECT common_get_table_id_by_name(drugs,'drug_2') INTO drug_2;
 
-
---REPORT#1
-SELECT util_report_1_generate() INTO fe_report_1;
-
-expected_rpt_geo_output=format('['||
+--MEDICAL FORMS
+expected_output=format('['||
     '{"indication_name":"indication_1","indication_abbre":"Ind1","benefit_name":"Pharmacy","drug_id":%1$s,"criteria_restriction_name":"PA - Age - criteria_age_1","criteria_restriction_short_name":"criteria_age_1","market_name":"Massachusetts","lives":100,"health_plan_count":1,"provider_count":0,"total_pharmacy_lives":200,"total_medical_lives":0,"total_health_plan_count":2,"total_provider_count":0},'||
     '{"indication_name":"indication_1","indication_abbre":"Ind1","benefit_name":"Pharmacy","drug_id":%1$s,"criteria_restriction_name":"PA - Clinical - criteria_clinical_1","criteria_restriction_short_name":"criteria_clinical_1","market_name":"Massachusetts","lives":100,"health_plan_count":1,"provider_count":0,"total_pharmacy_lives":200,"total_medical_lives":0,"total_health_plan_count":2,"total_provider_count":0},'||
     '{"indication_name":"indication_1","indication_abbre":"Ind1","benefit_name":"Pharmacy","drug_id":%1$s,"criteria_restriction_name":"PA - Diagnosis - criteria_diagnosis_1","criteria_restriction_short_name":"criteria_diagnosis_1","market_name":"Massachusetts","lives":100,"health_plan_count":1,"provider_count":0,"total_pharmacy_lives":200,"total_medical_lives":0,"total_health_plan_count":2,"total_provider_count":0},'||
@@ -34,9 +34,9 @@ expected_rpt_geo_output=format('['||
     '{"indication_name":"indication_1","indication_abbre":"Ind1","benefit_name":"Pharmacy","drug_id":%2$s,"criteria_restriction_name":"ST - Double - custom_option_1 AND  custom_option_2","criteria_restriction_short_name":"custom_option_1 AND  custom_option_2","market_name":"Massachusetts","lives":100,"health_plan_count":1,"provider_count":0,"total_pharmacy_lives":200,"total_medical_lives":0,"total_health_plan_count":2,"total_provider_count":0}'||
     ']',drug_1,drug_2);
 
-PERFORM res_rpt_geo_validate_data(fe_report_1, pharmacy_benefit_type, state_market_type, expected_rpt_geo_output);
+PERFORM res_rpt_geo_validate_data(report_id, pharmacy_benefit_type, state_market_type, expected_output);
 
 success:=true;
-return success;
+RETURN success;
 END
 $$ LANGUAGE plpgsql;
