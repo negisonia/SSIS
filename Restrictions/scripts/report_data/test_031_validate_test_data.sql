ee42e1bf09297a3daed800a3c140bd5a7e767d84
vsansilvestre <lema017@gmail.com>
2015-10-13 15:25:42 -0600

    adding drill down  for Connecticut

--- Restrictions/scripts/report_data/test_031_validate_test_data.sql
+++ Restrictions/scripts/report_data/test_031_validate_test_data.sql
@@ -20,6 +20,7 @@ ind1_pa_unspecified INTEGER;
 ind1_pa_past_custom_option_1 INTEGER;
 ind1_pa_st_double_co_1_co_2 INTEGER;
 massachusetts_market_id INTEGER;
+connecticut_market_id INTEGER;
 
 BEGIN
 
@@ -36,9 +37,12 @@ SELECT common_get_dim_criteria_restriction(indication_1,'Pharmacy','PA - Unspeci
 SELECT common_get_dim_criteria_restriction(indication_1,'Pharmacy','PA/ST - Single','custom_option_1') INTO ind1_pa_past_custom_option_1;
 SELECT common_get_dim_criteria_restriction(indication_1,'Pharmacy','ST - Double','custom_option_1 AND  custom_option_2') INTO ind1_pa_st_double_co_1_co_2;
 SELECT common_get_table_id_by_name('states','Massachusetts') INTO massachusetts_market_id;
+SELECT common_get_table_id_by_name('states','Connecticut') INTO connecticut_market_id;
 
 --REPORT#1
 SELECT util_report_1_generate() INTO fe_report_1;
+
+-- Massachusetts
 expected_rpt_geo_output='['||
 '{"indication_name":"indication_1","indication_abbre":"Ind1","drug_name":"drug_1","lives":100,"health_plan_count":1,"provider_count":0,"total_pharmacy_lives":200,"total_medical_lives":0,"total_health_plan_count":2,"total_provider_count":1,"benefit_name":"Pharmacy","criteria_restriction_name":"PA - Diagnosis - criteria_diagnosis_1","criteria_restriction_short_name":"criteria_diagnosis_1","market_name":"Middlesex"}'
 ||']';
@@ -89,6 +93,18 @@ expected_rpt_geo_output='['||
 '{"indication_name":"indication_1","indication_abbre":"Ind1","drug_name":"drug_2","lives":100,"health_plan_count":1,"provider_count":1,"total_pharmacy_lives":200,"total_medical_lives":0,"total_health_plan_count":2,"total_provider_count":0,"benefit_name":"Pharmacy","criteria_restriction_name":"ST - Double - custom_option_1 AND  custom_option_2","criteria_restriction_short_name":"custom_option_1 AND  custom_option_2","market_name":"Middlesex"}'||
 ']';
 PERFORM res_rpt_geo_drill_state_validate_data(fe_report_1, pharmacy_benefit_type, county_market_type, ind1_pa_st_double_co_1_co_2, drug_2, state_market_type, massachusetts_market_id, expected_rpt_geo_output);
+
+
+-- Connecticut
+expected_rpt_geo_output='['||
+'{"indication_name":"indication_1","indication_abbre":"Ind1","drug_name":"drug_2","lives":100,"health_plan_count":1,"provider_count":1,"total_pharmacy_lives":100,"total_medical_lives":0,"total_health_plan_count":1,"total_provider_count":0,"benefit_name":"Pharmacy","criteria_restriction_name":"PA - Unspecified - Criteria Unspecified","criteria_restriction_short_name":"Criteria Unspecified","market_name":"Middlesex"}'||
+']';
+PERFORM res_rpt_geo_drill_state_validate_data(fe_report_1, pharmacy_benefit_type, county_market_type, ind1_pa_unspecified, drug_2, state_market_type, connecticut_market_id, expected_rpt_geo_output);
+
+expected_rpt_geo_output='['||
+'{"indication_name":"indication_1","indication_abbre":"Ind1","drug_name":"drug_2","lives":100,"health_plan_count":1,"provider_count":1,"total_pharmacy_lives":100,"total_medical_lives":0,"total_health_plan_count":1,"total_provider_count":0,"benefit_name":"Pharmacy","criteria_restriction_name":"QL - criteria_ql_1","criteria_restriction_short_name":"criteria_ql_1","market_name":"Middlesex"}'||
+']';
+PERFORM res_rpt_geo_drill_state_validate_data(fe_report_1, pharmacy_benefit_type, county_market_type, ind1_pa_ql, drug_2, state_market_type, connecticut_market_id, expected_rpt_geo_output);
 
 
 success:=true;
