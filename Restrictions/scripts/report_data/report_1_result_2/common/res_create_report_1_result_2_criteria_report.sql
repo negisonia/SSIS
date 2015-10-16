228d7a4b67ab13d25c6a0202c0b3800cca0783ee
vsansilvestre <lema017@gmail.com>
2015-10-16 10:21:58 -0600

    adding changes to craete criteria report 1 result 2

--- Restrictions/scripts/report_data/report_1_result_2/common/res_create_report_1_result_2_criteria_report.sql
+++ Restrictions/scripts/report_data/report_1_result_2/common/res_create_report_1_result_2_criteria_report.sql
@@ -23,7 +23,7 @@ report1 INTEGER;
 
 criteria_report_type CONSTANT INTEGER:=1;
 step_report_type CONSTANT INTEGER:=2;
-county_geo_type_id CONSTANT INTEGER:=1;
+state_geo_type_id CONSTANT INTEGER:=1;
 
 user_id CONSTANT INTEGER:=1;
 
@@ -34,7 +34,7 @@ fe_report_1 INTEGER;
 summary_table_output VARCHAR;
 expected_summary_table_output VARCHAR;
 massachusetts_state_id INTEGER;
-county_ids INTEGER[];
+state_ids INTEGER[];
 
 BEGIN
 
@@ -52,8 +52,7 @@ SELECT common_get_table_id_by_name('indications','indication_1') INTO indication
 SELECT common_get_table_id_by_name('drugs','drug_1') INTO drug_1;
 SELECT common_get_table_id_by_name('drugs','drug_2') INTO drug_2;
 
-SELECT common_get_table_id_by_name('states','Massachusetts') INTO massachusetts_state_id;
-SELECT array(select id from counties where state_id=massachusetts_state_id) INTO county_ids;
+SELECT array(common_get_table_id_by_name('states','Massachusetts')) INTO state_ids;
 
 --RETRIEVE RESTRICTIONS
 
@@ -68,8 +67,7 @@ health_plan_types_array:= ARRAY[commercial_hpt,hix_hpt];
 restrictions_array:= ARRAY[ind1_m_unspecified,ind1_m_age_1,ind1_m_st_custom_option_2,ind1_m_criteria_diagnosis_3];
 empty_array:= ARRAY[]::integer[];
 
---TODO ver como se inserta county info
-SELECT create_criteria_report( report1, user_id , criteria_report_type, NULL, county_geo_type_id , NULL, NULL, NULL, drugs_array, health_plan_types_array , 'County',empty_array, NULL, restrictions_array, NULL, NULL,county_ids) INTO fe_report_1;
+SELECT create_criteria_report( report1, user_id, criteria_report_type, NULL, state_geo_type_id, TRUE, FALSE, FALSE, drugs_array, health_plan_types_array , 'State', empty_array, NULL, restrictions_array, state_ids, NULL, NULL) INTO fe_report_1;
 return fe_report_1;
 END
 $$ LANGUAGE plpgsql;
