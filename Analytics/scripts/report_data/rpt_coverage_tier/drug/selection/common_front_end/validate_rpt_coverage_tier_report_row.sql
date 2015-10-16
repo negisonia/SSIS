e0d6880061db102802e14d98b38fb2bbe46f5038
Ignacio <imonge@growthaccelerationpartners.com>
2015-10-16 13:46:36 -0600

    ANA2370 Add tier summary scenario 1 test cases

---
+++ Analytics/scripts/report_data/rpt_coverage_tier/drug/selection/common_front_end/validate_rpt_coverage_tier_report_row.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION ana_rpt_coverage_tier_drug_validate_report_row(expected_value varchar, test_number varchar, drug_name varchar, dim_tier_name varchar, criteria_report_id integer)
+RETURNS boolean AS $$
+DECLARE
+  success boolean DEFAULT FALSE;
+  actual_value varchar;
+  current_month_int INTEGER;
+  report_select_columns varchar;
+BEGIN
+
+-- Current Month
+SELECT get_current_month() INTO current_month_int;
+
+report_select_columns  = 'drug_name, dim_tier_name, dim_tier_id, dim_tier_type_id, is_tier_preferred, avg_copay, lis_lives, total_lis_lives, lives, total_lives, health_plan_count, total_health_plan_count';
+--Query the actual value
+SELECT calculate_report_value_json(report_select_columns, get_report_name_call('rpt_coverage_tier_drug', ARRAY[criteria_report_id,current_month_int]), 'drug_name=''' || drug_name || ''' AND dim_tier_name=''' || dim_tier_name || ''' ') INTO actual_value;
+
+PERFORM validate_comparison_values_varchar(actual_value, expected_value,'ana_rpt_coverage_tier_drug_test_'|| test_number ||'_validate_data-error: EXPECTED FOR ROW RESULTS TO BE ');
+
+success:=true;
+RETURN success;
+END
+$$ LANGUAGE plpgsql;
