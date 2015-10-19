3cc741b0ca19b53983a2afab4077489a457640e6
ignacio753 <ignacio@its-Mac-mini-47.local>
2015-10-19 11:59:22 -0600

    Minor update

--- Restrictions/scripts/report_data/report_1_result_2/common/res_common_report_1_result_2_rpt_health_plan_type_medical.sql
+++ Restrictions/scripts/report_data/report_1_result_2/common/res_common_report_1_result_2_rpt_health_plan_type_medical.sql
@@ -3,17 +3,17 @@ RETURNS boolean AS $$
 DECLARE
 success BOOLEAN DEFAULT FALSE;
 medical_view CONSTANT integer:=2;
-expected_medical_summary_table_output VARCHAR;
+expected_output VARCHAR;
 BEGIN
 
 --VALIDATE SUMMARY TABLE
-expected_medical_summary_table_output= '['||
+expected_output= '['||
 '{"indication_name":"indication_1","indication_abbre":"Ind1","benefit_name":"Medical","restriction_name":"Diagnosis","criteria_restriction_name":"Diagnosis - criteria_diagnosis_3","criteria_restriction_short_name":"criteria_diagnosis_3","health_plan_type_name":"hix","drug_name":"drug_2","lives":100,"health_plan_count":0,"provider_count":1,"total_pharmacy_lives":0,"total_medical_lives":100,"total_health_plan_count":0,"total_provider_count":1},'||
 '{"indication_name":"indication_1","indication_abbre":"Ind1","benefit_name":"Medical","restriction_name":"Age","criteria_restriction_name":"Age - criteria_age_1","criteria_restriction_short_name":"criteria_age_1","health_plan_type_name":"commercial","drug_name":"drug_2","lives":100,"health_plan_count":0,"provider_count":1,"total_pharmacy_lives":0,"total_medical_lives":100,"total_health_plan_count":0,"total_provider_count":1},'||
 '{"indication_name":"indication_1","indication_abbre":"Ind1","benefit_name":"Medical","restriction_name":"ST - Single","criteria_restriction_name":"ST - Single - custom_option_2","criteria_restriction_short_name":"custom_option_2","health_plan_type_name":"hix","drug_name":"drug_2","lives":100,"health_plan_count":0,"provider_count":1,"total_pharmacy_lives":0,"total_medical_lives":100,"total_health_plan_count":0,"total_provider_count":1}'||
 ']';
 
-PERFORM res_rpt_health_plan_type_validate_data(report_id, medical_view, expected_medical_summary_table_output);
+PERFORM res_rpt_health_plan_type_validate_data(report_id, medical_view, expected_output);
 
 success=true;
 return success;
