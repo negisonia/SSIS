b9a4c03a4be1c9cd5ad1ffc5ea96ca3b3b0c3df4
vsansilvestregap <vsansilvestre@wearegap.com>
2015-10-19 11:59:11 -0600

    adding common utilitary function for result 2 test 37

--- Restrictions/Scripts/report_data/report_1_result_2/common/res_common_report_1_result_2_rpt_drugs_table_pharmacy.sql
+++ Restrictions/Scripts/report_data/report_1_result_2/common/res_common_report_1_result_2_rpt_drugs_table_pharmacy.sql
@@ -2,12 +2,12 @@ CREATE OR REPLACE FUNCTION res_common_report_1_result_2_rpt_drugs_table_pharmacy
 RETURNS boolean AS $$
 DECLARE
 success BOOLEAN DEFAULT FALSE;
-pharmacy_view CONSTANT integer:=1;
+pharmacy_view CONSTANT INTEGER:=1;
 expected_rpt_drug_output VARCHAR;
 BEGIN
 
 --VALIDATE RPT_DRUG pharmacy
-expected_rpt_drug_output= format('%1$s',report_id);
+expected_rpt_drug_output= null;
 
 PERFORM res_rpt_drug_validate_data(report_id, pharmacy_view ,expected_rpt_drug_output);
 
--- Restrictions/Scripts/report_data/report_1_result_2/res_report_1_result_2_test_37_validate_test_data.sql
+++ Restrictions/Scripts/report_data/report_1_result_2/res_report_1_result_2_test_37_validate_test_data.sql
@@ -3,14 +3,11 @@ RETURNS boolean AS $$
 DECLARE
 success BOOLEAN DEFAULT FALSE;
 fe_report_1 INTEGER;
-pharmacy_view CONSTANT integer:=1;
-medical_view CONSTANT integer:=2;
-expected_pharmacy_summary_table_output VARCHAR;
-expected_medical_summary_table_output VARCHAR;
 BEGIN
 --REPORT#1
 SELECT res_create_report_1_result_2_criteria_report() INTO fe_report_1;
 PERFORM res_common_report_1_result_2_rpt_drugs_table_pharmacy(fe_report_1);
+PERFORM res_common_report_1_result_2_rpt_drugs_table_medical(fe_report_1);
 
 success=true;
 return success;
