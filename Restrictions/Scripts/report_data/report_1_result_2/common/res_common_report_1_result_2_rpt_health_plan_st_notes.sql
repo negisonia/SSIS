42efad8a3b3b5aa121f15203a271d34d6acc112f
vsansilvestregap <vsansilvestre@wearegap.com>
2015-10-19 16:03:19 -0600

    adding test ca 27

--- Restrictions/Scripts/report_data/report_1_result_2/common/res_common_report_1_result_2_rpt_health_plan_st_notes.sql
+++ Restrictions/Scripts/report_data/report_1_result_2/common/res_common_report_1_result_2_rpt_health_plan_st_notes.sql
@@ -14,7 +14,10 @@ SELECT common_get_table_id_by_name('health_plan_types','hix') INTO hix_hpt;
 SELECT common_get_table_id_by_name('providers','provider_1') INTO provider_id;
 
 --ST HIX DRUG 2
-expected_output= '';
+expected_output= '['||
+'{"indication_name":"indication_1","dim_criterion_type_id":3,"criterion_name":"ST - Double - custom_option_1^1 AND custom_option_2^2","note_position":1,"notes":"Drug1 notes: notes for drug 1"},'||
+'{"indication_name":"indication_1","dim_criterion_type_id":3,"criterion_name":"ST - Double - custom_option_1^1 AND custom_option_2^2","note_position":2,"notes":"Drug2 notes: notes for drug 2"}'||
+']';
 PERFORM res_rpt_health_plan_notes_validate_data(report_id, provider_id, hix_hpt, drug_2_id, st_dim_criterion_type, expected_output);
 
 success=true;
