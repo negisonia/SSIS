4afe750a56b72c01fcecb2601e5362855c78dfac
vsansilvestregap <vsansilvestre@wearegap.com>
2015-10-26 09:55:45 -0600

    adding cleanup commands

--- Common/front_end/clear_test_data.sql
+++ Common/front_end/clear_test_data.sql
@@ -5,10 +5,14 @@ success boolean DEFAULT false;
 BEGIN
 
   --CLEAR TABLES
+  TRUNCATE TABLE counties_criteria_reports CASCADE;
   TRUNCATE TABLE criteria_reports_drugs CASCADE;
   TRUNCATE TABLE criteria_reports_health_plan_types CASCADE;
   TRUNCATE TABLE criteria_reports_markets CASCADE;
+  TRUNCATE TABLE criteria_reports_metro_stat_areas CASCADE;
   TRUNCATE TABLE criteria_reports CASCADE;
+  TRUNCATE TABLE criteria_reports_states CASCADE;
+  TRUNCATE TABLE report_selections CASCADE;
   TRUNCATE TABLE criteria_reports_dim_criteria_restriction CASCADE;
   
   --CLEAR SEQUENCES
