7e97ae666aa5ea00b4e7cbf5e4f838aefe623fdc
vsansilvestre <lema017@gmail.com>
2015-11-04 14:48:14 -0600

    adding new test 98

---
+++ Restrictions/Scripts/report_data/report_1_result_3/common/res_ca_create_report_1_result_3_criteria_report.sql
@@ -0,0 +1,59 @@
+CREATE OR REPLACE FUNCTION res_ca_create_report_1_result_3_criteria_report() --FF NEW DB
+RETURNS INTEGER AS $$
+DECLARE
+
+indication_1 INTEGER;
+
+criteria_report_type CONSTANT INTEGER:=1;
+county_geo_type_id CONSTANT INTEGER:=1;
+
+drug_1_id INTEGER;
+drug_2_id INTEGER;
+criteria_report_id INTEGER;
+
+new_report_id INTEGER;
+existing_client_id INTEGER;
+existing_custom_account_id INTEGER;
+
+ind1_m_unspecified INTEGER;
+ind1_m_age_1 INTEGER;
+ind1_m_st_custom_option_2 INTEGER;
+ind1_m_criteria_diagnosis_3 INTEGER;
+
+restrictions_array INTEGER[];
+
+drugs CONSTANT VARCHAR:='drugs';
+massachusetts_state_id INTEGER;
+county_ids INTEGER[];
+
+BEGIN
+
+--GET the input data
+
+--RETRIEVE INDICATIONS
+SELECT common_get_table_id_by_name('indications','indication_1') INTO indication_1;
+
+--RETRIEVE DRUGS
+SELECT common_get_table_id_by_name(drugs, 'drug_1') INTO drug_1_id;
+SELECT common_get_table_id_by_name(drugs, 'drug_2') INTO drug_2_id;
+
+--RETRIEVE RESTRICTIONS
+SELECT common_get_dim_criteria_restriction(indication_1,'Medical','Unspecified','Criteria Unspecified') INTO ind1_m_unspecified;
+SELECT common_get_dim_criteria_restriction(indication_1,'Medical','Age','criteria_age_1') INTO ind1_m_age_1;
+SELECT common_get_dim_criteria_restriction(indication_1,'Medical','ST - Single','custom_option_2') INTO ind1_m_st_custom_option_2;
+SELECT common_get_dim_criteria_restriction(indication_1,'Medical','Diagnosis','criteria_diagnosis_3') INTO ind1_m_criteria_diagnosis_3;
+
+restrictions_array:= ARRAY[ind1_m_unspecified, ind1_m_age_1, ind1_m_st_custom_option_2, ind1_m_criteria_diagnosis_3];
+
+SELECT report_id from criteria_restriction_reports where report_name = 'report_1' INTO new_report_id;
+SELECT common_get_table_id_by_name('clients', 'client_2') INTO existing_client_id;
+SELECT custom_account_id from custom_accounts where name = 'Custom_Account_2' and client_id = existing_client_id INTO existing_custom_account_id;
+
+SELECT common_get_table_id_by_name('states','Massachusetts') INTO massachusetts_state_id;
+SELECT array(select id from counties where state_id=massachusetts_state_id) INTO county_ids;
+
+SELECT create_criteria_report(new_report_id,0,criteria_report_type,0,county_geo_type_id,FALSE,FALSE,FALSE,ARRAY[drug_1_id,drug_2_id],ARRAY[]::integer[],'County',ARRAY[]::integer[],existing_custom_account_id,restrictions_array,NULL,NULL,county_ids) INTO criteria_report_id;
+
+RETURN criteria_report_id;
+END
+$$ LANGUAGE plpgsql;
---
+++ Restrictions/Scripts/report_data/report_1_result_3/common/res_create_report_1_result_3_criteria_report.sql
@@ -0,0 +1,67 @@
+CREATE OR REPLACE FUNCTION res_create_report_1_result_3_criteria_report() --FRONT END
+RETURNS INTEGER AS $$
+DECLARE
+
+indication_1 INTEGER;
+
+drug_1 INTEGER;
+drug_2 INTEGER;
+
+rep_1_group_1_pa INTEGER;
+rep_1_group_3_pa INTEGER;
+rep_1_group_1_m INTEGER;
+rep_1_group_3_m INTEGER;
+
+drugs_array INTEGER[];
+health_plan_types_array INTEGER[];
+restrictions_array INTEGER[];
+empty_array INTEGER[];
+
+report1 INTEGER;
+fe_report_1 INTEGER;
+
+criteria_report_type CONSTANT INTEGER:=1;
+state_geo_type_id CONSTANT INTEGER:=2;
+
+user_id CONSTANT INTEGER:=1;
+
+commercial_hpt INTEGER;
+hix_hpt INTEGER;
+
+state_ids INTEGER[];
+
+BEGIN
+
+--RETRIEVE HEALTH PLAN TYPES
+SELECT common_get_table_id_by_name('health_plan_types','commercial') INTO commercial_hpt;
+SELECT common_get_table_id_by_name('health_plan_types','hix') INTO hix_hpt;
+
+--RETRIEVE REPORTS
+SELECT ccr.report_id INTO report1 FROM criteria_restriction_reports ccr WHERE ccr.report_name='report_1';
+
+--RETRIEVE INDICATIONS
+SELECT common_get_table_id_by_name('indications','indication_1') INTO indication_1;
+
+--RETRIEVE DRUGS
+SELECT common_get_table_id_by_name('drugs','drug_1') INTO drug_1;
+SELECT common_get_table_id_by_name('drugs','drug_2') INTO drug_2;
+
+SELECT array(select distinct s.id FROM states s WHERE s.is_active=true) INTO state_ids;
+
+--RETRIEVE RESTRICTIONS
+
+SELECT common_get_custom_dim_criteria(indication_1, 'Pharmacy', 'rep_1_group_1', 'rep_1_group_1',6,3) INTO rep_1_group_1_pa;
+SELECT common_get_custom_dim_criteria(indication_1, 'Pharmacy', 'rep_1_group_3', 'rep_1_group_3',6,3) INTO rep_1_group_3_pa;
+SELECT common_get_custom_dim_criteria(indication_1, 'Medical', 'rep_1_group_1', 'rep_1_group_1',2,3) INTO rep_1_group_1_m;
+SELECT common_get_custom_dim_criteria(indication_1, 'Medical', 'rep_1_group_3', 'rep_1_group_3',2,3) INTO rep_1_group_3_m;
+
+--REPORT#1
+drugs_array:= ARRAY[drug_1,drug_2];
+health_plan_types_array:= ARRAY[commercial_hpt,hix_hpt];
+restrictions_array:= ARRAY[rep_1_group_1_pa,rep_1_group_3_pa,rep_1_group_1_m,rep_1_group_3_m];
+empty_array:= ARRAY[]::integer[];
+
+SELECT create_criteria_report( report1, user_id ,criteria_report_type, NULL, state_geo_type_id, TRUE, FALSE, FALSE, drugs_array, health_plan_types_array, 'State', empty_array, NULL, restrictions_array, states_ids, NULL, NULL) INTO fe_report_1;
+return fe_report_1;
+END
+$$ LANGUAGE plpgsql;
