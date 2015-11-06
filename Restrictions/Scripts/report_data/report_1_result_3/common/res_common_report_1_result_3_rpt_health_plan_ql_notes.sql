fffec5da3031dce9f3b271fc8a5e07ca2cefb7a7
vsansilvestre <lema017@gmail.com>
2015-11-06 10:48:14 -0600

    adding test ca90  res104

---
+++ Restrictions/Scripts/report_data/report_1_result_3/common/res_common_report_1_result_3_rpt_health_plan_ql_notes.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE FUNCTION res_common_report_1_result_3_rpt_health_plan_ql_notes(report_id INTEGER) -- REPORT FRONT END
+RETURNS boolean AS $$
+DECLARE
+success BOOLEAN DEFAULT FALSE;
+provider_id INTEGER;
+commercial_hpt INTEGER;
+hix_hpt INTEGER;
+dim_restriction_type_id INTEGER:=4;
+drug_1_id INTEGER;
+drug_2_id INTEGER;
+expected_output VARCHAR;
+BEGIN
+
+SELECT common_get_table_id_by_name('drugs','drug_1') INTO drug_1_id;
+SELECT common_get_table_id_by_name('drugs','drug_2') INTO drug_2_id;
+
+--RETRIEVE HEALTH PLAN TYPE
+SELECT common_get_table_id_by_name('health_plan_types','commercial') INTO commercial_hpt;
+
+SELECT common_get_table_id_by_name('providers','provider_1') INTO provider_id;
+
+--MEDICAL COMMERCIAL DRUG 2
+expected_output= '['||
+'{"indication_name":"indication_1","dim_criterion_type_id":1,"criterion_name":"criteria_ql_1","note_position":1,"notes":"1 tabs per 10 days\\n\\nql message"}'||
+']';
+PERFORM res_rpt_health_plan_notes_validate_data(report_id, provider_id, commercial_hpt, drug_2_id, dim_restriction_type_id, expected_output);
+
+--MEDICAL COMMERCIAL DRUG 1
+expected_output= '['||
+'{"indication_name":"indication_1","dim_criterion_type_id":1,"criterion_name":"criteria_ql_1","note_position":1,"notes":"2 tabs per 10 week"}'||
+']';
+PERFORM res_rpt_health_plan_notes_validate_data(report_id, provider_id, commercial_hpt, drug_1_id, dim_restriction_type_id, expected_output);
+
+success=true;
+return success;
+END
+$$ LANGUAGE plpgsql;
