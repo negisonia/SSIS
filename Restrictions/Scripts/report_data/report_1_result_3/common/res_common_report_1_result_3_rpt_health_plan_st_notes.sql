f415f0be53176ae9b0fedc37b19774192d39ed1b
vsansilvestre <lema017@gmail.com>
2015-11-06 10:56:15 -0600

    adding test ca91  res105

---
+++ Restrictions/Scripts/report_data/report_1_result_3/common/res_common_report_1_result_3_rpt_health_plan_st_notes.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION res_common_report_1_result_3_rpt_health_plan_st_notes(report_id INTEGER) -- REPORT FRONT END
+RETURNS boolean AS $$
+DECLARE
+success BOOLEAN DEFAULT FALSE;
+provider_id INTEGER;
+hix_hpt INTEGER;
+dim_restriction_type_id INTEGER:=3;
+drug_2_id INTEGER;
+expected_output VARCHAR;
+BEGIN
+
+SELECT common_get_table_id_by_name('drugs','drug_2') INTO drug_2_id;
+
+--RETRIEVE HEALTH PLAN TYPE
+SELECT common_get_table_id_by_name('health_plan_types','hix') INTO hix_hpt;
+
+SELECT common_get_table_id_by_name('providers','provider_1') INTO provider_id;
+
+--ST HIX DRUG 2
+expected_output= '['||
+'{"indication_name":"indication_1","dim_criterion_type_id":2,"criterion_name":"ST - Double - custom_option_1^1 AND custom_option_2^2","note_position":1,"notes":"Drug1 notes: notes for drug 1"},'||
+'{"indication_name":"indication_1","dim_criterion_type_id":2,"criterion_name":"ST - Double - custom_option_1^1 AND custom_option_2^2","note_position":2,"notes":"Drug2 notes: notes for drug 2"}'||
+']';
+PERFORM res_rpt_health_plan_notes_validate_data(report_id, provider_id, hix_hpt, drug_2_id, dim_restriction_type_id, expected_output);
+
+
+success=true;
+return success;
+END
+$$ LANGUAGE plpgsql;
