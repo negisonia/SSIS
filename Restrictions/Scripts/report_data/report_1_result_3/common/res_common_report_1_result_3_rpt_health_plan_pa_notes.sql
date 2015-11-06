6d4edbeb569bb1439bd2d2793c212f0c96d14a34
vsansilvestre <lema017@gmail.com>
2015-11-06 10:20:17 -0600

    removing comments

--- Restrictions/Scripts/report_data/report_1_result_3/common/res_common_report_1_result_3_rpt_health_plan_pa_notes.sql
+++ Restrictions/Scripts/report_data/report_1_result_3/common/res_common_report_1_result_3_rpt_health_plan_pa_notes.sql
@@ -20,22 +20,22 @@ SELECT common_get_table_id_by_name('providers','provider_1') INTO provider_id;
 
 
 --DRUG1 COMMERCIAL
---expected_output= '['||
---'{"indication_name":"indication_1","dim_criterion_type_id":1,"criterion_name":"criteria_age_1","note_position":1,"notes":"age restriction: l:10 up:30"},'||
---'{"indication_name":"indication_1","dim_criterion_type_id":1,"criterion_name":"criteria_clinical_1","note_position":1,"notes":"long message 500 characters"},'||
---'{"indication_name":"indication_1","dim_criterion_type_id":1,"criterion_name":"criteria_diagnosis_1","note_position":1,"notes":""},'||
---'{"indication_name":"indication_1","dim_criterion_type_id":1,"criterion_name":"criteria_diagnosis_3","note_position":1,"notes":"long message 100 characters"},'||
---'{"indication_name":"indication_1","dim_criterion_type_id":2,"criterion_name":"PA/ST - Single - custom_option_1^1","note_position":1,"notes":"Drug1 notes: long message 500 characters"}'||
---']';
---
---PERFORM res_rpt_health_plan_notes_validate_data(report_id, provider_id, commercial_plan_type_id, drug_1_id, dim_restriction_type_id, expected_output);
+expected_output= '['||
+'{"indication_name":"indication_1","dim_criterion_type_id":1,"criterion_name":"criteria_age_1","note_position":1,"notes":"age restriction: l:10 up:30"},'||
+'{"indication_name":"indication_1","dim_criterion_type_id":1,"criterion_name":"criteria_clinical_1","note_position":1,"notes":"long message 500 characters"},'||
+'{"indication_name":"indication_1","dim_criterion_type_id":1,"criterion_name":"criteria_diagnosis_1","note_position":1,"notes":""},'||
+'{"indication_name":"indication_1","dim_criterion_type_id":1,"criterion_name":"criteria_diagnosis_3","note_position":1,"notes":"long message 100 characters"},'||
+'{"indication_name":"indication_1","dim_criterion_type_id":2,"criterion_name":"PA/ST - Single - custom_option_1^1","note_position":1,"notes":"Drug1 notes: long message 500 characters"}'||
+']';
+
+PERFORM res_rpt_health_plan_notes_validate_data(report_id, provider_id, commercial_plan_type_id, drug_1_id, dim_restriction_type_id, expected_output);
 
 
 --DRUG1 HIX
---expected_output= '['||
---'{"indication_name":"indication_1","dim_criterion_type_id":1,"criterion_name":"Criteria Unspecified","note_position":1,"notes":"Notes"}'||
---']';
---PERFORM res_rpt_health_plan_notes_validate_data(report_id, provider_id, hix_plan_type_id, drug_1_id, dim_restriction_type_id, expected_output);
+expected_output= '['||
+'{"indication_name":"indication_1","dim_criterion_type_id":1,"criterion_name":"Criteria Unspecified","note_position":1,"notes":"Notes"}'||
+']';
+PERFORM res_rpt_health_plan_notes_validate_data(report_id, provider_id, hix_plan_type_id, drug_1_id, dim_restriction_type_id, expected_output);
 
 
 --DRUG2 HIX
