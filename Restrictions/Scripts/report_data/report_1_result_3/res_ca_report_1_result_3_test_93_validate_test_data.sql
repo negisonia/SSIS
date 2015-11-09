57926b4f9e6efc8e4f5a4657f1fddafd982c4292
vsansilvestre <lema017@gmail.com>
2015-11-09 11:09:09 -0600

    adding test ca93 res 107

---
+++ Restrictions/Scripts/report_data/report_1_result_3/res_ca_report_1_result_3_test_93_validate_test_data.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION res_ca_report_1_result_3_test_93_validate_test_data() -- REPORT FRONT END
+RETURNS boolean AS $$
+DECLARE
+success BOOLEAN DEFAULT FALSE;
+criteria_report_id INTEGER;
+BEGIN
+-- Create Criteria Report Id
+SELECT res_ca_create_report_1_result_3_criteria_report() INTO criteria_report_id;
+PERFORM res_common_report_1_result_3_rpt_provider_notes(criteria_report_id);
+success=true;
+return success;;
+END
+$$ LANGUAGE plpgsql;
