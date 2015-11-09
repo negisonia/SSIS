38d453ad0f62df8dc8915f1bdc762808d1b097bd
vsansilvestre <lema017@gmail.com>
2015-11-09 13:39:15 -0600

    adding test ca93 res 108

--- Restrictions/Scripts/report_data/report_1_result_3/res_ca_report_1_result_3_test_95_validate_test_data.sql
+++ Restrictions/Scripts/report_data/report_1_result_3/res_ca_report_1_result_3_test_95_validate_test_data.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION res_ca_report_1_result_3_test_93_validate_test_data() -- REPORT FRONT END
+CREATE OR REPLACE FUNCTION res_ca_report_1_result_3_test_95_validate_test_data() -- REPORT FRONT END
 RETURNS boolean AS $$
 DECLARE
 success BOOLEAN DEFAULT FALSE;
@@ -6,7 +6,7 @@ criteria_report_id INTEGER;
 BEGIN
 -- Create Criteria Report Id
 SELECT res_ca_create_report_1_result_3_criteria_report() INTO criteria_report_id;
-PERFORM res_common_report_1_result_3_rpt_provider_notes(criteria_report_id);
+PERFORM res_common_report_1_result_3_rpt_pharmacy_document(criteria_report_id);
 success:=true;
 return success;
 END
