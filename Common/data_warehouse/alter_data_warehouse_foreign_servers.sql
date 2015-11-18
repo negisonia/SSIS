d5fff9d54f3d9d351ed6320129e5a53989b76844
vsansilvestregap <vsansilvestre@wearegap.com>
2015-11-18 15:26:09 -0600

    adding changes to data entry creation script

--- Common/data_warehouse/alter_data_warehouse_foreign_servers.sql
+++ Common/data_warehouse/alter_data_warehouse_foreign_servers.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION alter_datawarehouse_foreign_servers(host_name varchar, temp_ff_new_db_name varchar, temp_data_entry_db_name varchar, temp_admin_db_name varchar, postgres_password varchar) --Data Warehouse
+saCREATE OR REPLACE FUNCTION alter_datawarehouse_foreign_servers(host_name varchar, temp_ff_new_db_name varchar, temp_data_entry_db_name varchar, temp_admin_db_name varchar, postgres_password varchar) --Data Warehouse
 RETURNS void AS $$
 DECLARE
   
@@ -15,6 +15,10 @@ BEGIN
   EXECUTE 'alter server foreign_admin options (set host ''' || host_name || ''')';
   EXECUTE 'alter server foreign_admin options (set dbname ''' || temp_admin_db_name || ''')';
   EXECUTE 'alter USER MAPPING FOR postgres SERVER foreign_admin OPTIONS (set user ''postgres'', set password ''' || postgres_password || ''')';
+
+  EXECUTE 'alter server foreign_ff_new_historical options (set host ''' || host_name || ''')';
+  EXECUTE 'alter server foreign_ff_new_historical options (set dbname ''' || temp_admin_db_name || ''')';
+  EXECUTE 'alter USER MAPPING FOR postgres SERVER foreign_ff_new_historical OPTIONS (set user ''postgres'', set password ''' || postgres_password || ''')';
 
 END
 $$ LANGUAGE plpgsql;
