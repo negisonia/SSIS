4bb67fda9cd99b4b1265bad27e1b857edb3c8179
vsansilvestre <lema017@gmail.com>
2015-11-26 13:19:46 -0600

    adding TEST ca 56

--- Restrictions/Scripts/report_data/report_1_result_4/common/res_common_report_1_result_4_rpt_provider_details_medical.sql
+++ Restrictions/Scripts/report_data/report_1_result_4/common/res_common_report_1_result_4_rpt_provider_details_medical.sql
@@ -14,20 +14,7 @@ SELECT common_get_table_id_by_name('providers','provider_1') INTO provider_id;
 SELECT common_get_table_id_by_name('health_plan_types','commercial') INTO commercial_plan_type_id;
 SELECT common_get_table_id_by_name('health_plan_types','hix') INTO hix_plan_type_id;
 
-expected_provider_details_table_output= format('[-'||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%2$s,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"Age - criteria_age_1"},'||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%3$s,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"Diagnosis - criteria_diagnosis_3"},'||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%3$s,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"ST - Single - custom_option_2"},'||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%3$s,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"Single - Single"},'||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%2$s,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_1"},'||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%2$s,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_3"},'||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%3$s,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_all"},'||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%3$s,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_all - rep_1_group_all - Single"},'||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%2$s,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_both"},'||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%3$s,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_both"},'||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%3$s,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_both - rep_1_group_both - Single"},'||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%2$s,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"rep_4_group_1"}'||
-']',provider_id,commercial_plan_type_id,hix_plan_type_id);
+expected_provider_details_table_output= '';
 PERFORM res_rpt_provider_details_validate_data(report_id, medical_view, expected_provider_details_table_output);
 
 success=true;
--- Restrictions/Scripts/report_data/report_1_result_4/common/res_common_report_1_result_4_rpt_provider_details_pa.sql
+++ Restrictions/Scripts/report_data/report_1_result_4/common/res_common_report_1_result_4_rpt_provider_details_pa.sql
@@ -14,62 +14,7 @@ SELECT common_get_table_id_by_name('providers','provider_1') INTO provider_id;
 SELECT common_get_table_id_by_name('health_plan_types','commercial') INTO commercial_plan_type_id;
 SELECT common_get_table_id_by_name('health_plan_types','hix') INTO hix_plan_type_id;
 
-expected_provider_details_table_output= format('[-'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"PA - Age - criteria_age_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"PA - Clinical - criteria_clinical_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"PA - Diagnosis - criteria_diagnosis_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"PA - Diagnosis - criteria_diagnosis_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"PA - Diagnosis - criteria_diagnosis_3"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"PA - Unspecified - Criteria Unspecified"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_1","criteria_restriction_name":"PA - Unspecified - Criteria Unspecified"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"PA/ST - Single - custom_option_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"PA/ST - Single - custom_option_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"QL - criteria_ql_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"QL - criteria_ql_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"ST - Double - custom_option_1 AND  custom_option_2"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"Double - Double"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"Double - Double"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"Single - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"Single - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"Single - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"Single - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_2"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_2"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_3"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_3"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_all"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_all - rep_1_group_all - Double"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_all - rep_1_group_all - Double"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_both"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_both"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_both"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_both"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_both - rep_1_group_both - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_both - rep_1_group_both - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_both - rep_1_group_both - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_both - rep_1_group_both - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_single - rep_1_group_single - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_single - rep_1_group_single - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_single - rep_1_group_single - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_single - rep_1_group_single - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_steps"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_steps"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_steps - rep_1_group_steps - Double"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_steps - rep_1_group_steps - Double"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_steps - rep_1_group_steps - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_steps - rep_1_group_steps - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"rep_1_group_steps - rep_1_group_steps - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"rep_1_group_steps - rep_1_group_steps - Single"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_1","criteria_restriction_name":"rep_4_group_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":1,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"rep_4_group_1"},'||
-'{"provider_id":1,"benefit_name":"Pharmacy","health_plan_type_id":13,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_1","criteria_restriction_name":"rep_4_group_1"}'||
-']',provider_id,commercial_plan_type_id,hix_plan_type_id);
+expected_provider_details_table_output= '';
 
 
 PERFORM res_rpt_provider_details_validate_data(report_id, pharmacy_view, expected_provider_details_table_output);
