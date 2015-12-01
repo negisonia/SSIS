964b484611bd2d418dff386d3576f4cb1e84515e
vsansilvestre <lema017@gmail.com>
2015-12-01 15:34:19 -0600

    refactor

--- Restrictions/Scripts/report_data/report_1_result_4/common/res_common_report_1_result_4_rpt_provider_details_medical.sql
+++ Restrictions/Scripts/report_data/report_1_result_4/common/res_common_report_1_result_4_rpt_provider_details_medical.sql
@@ -14,7 +14,9 @@ SELECT common_get_table_id_by_name('providers','provider_1') INTO provider_id;
 SELECT common_get_table_id_by_name('health_plan_types','commercial') INTO commercial_plan_type_id;
 SELECT common_get_table_id_by_name('health_plan_types','hix') INTO hix_plan_type_id;
 
-expected_provider_details_table_output= '';
+expected_provider_details_table_output= format('['||
+'{"provider_id":1,"benefit_name":"Medical","health_plan_type_id":%1$s,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"ST - Single - custom_option_2"}'||
+']',hix_plan_type_id);
 PERFORM res_rpt_provider_details_validate_data(report_id, medical_view, expected_provider_details_table_output);
 
 success=true;
