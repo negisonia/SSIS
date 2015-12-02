a6eab82f1c0dfd86150b42a6ff9b85f04202eeec
vsansilvestregap <vsansilvestre@wearegap.com>
2015-12-02 10:19:14 -0600

    Adding test ca 61 62 63 64

---
+++ Restrictions/Scripts/report_data/report_1_result_4/res_ca_report_1_result_4_test_62_validate_test_data.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION res_ca_report_1_result_4_test_62_validate_test_data() --REPORT FRONT END
+RETURNS boolean AS $$
+DECLARE
+success BOOLEAN DEFAULT FALSE;
+criteria_report_id INTEGER;
+BEGIN
+
+--CREATE CRITERIA REPORT
+SELECT res_ca_create_report_1_result_4_criteria_report() INTO criteria_report_id;
+PERFORM res_common_report_1_result_4_rpt_geo_medical(criteria_report_id);
+success:=true;
+return success;
+END
+$$ LANGUAGE plpgsql;
---
+++ Restrictions/Scripts/report_data/report_1_result_4/res_ca_report_1_result_4_test_63_validate_test_data.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION res_ca_report_1_result_4_test_63_validate_test_data() --REPORT FRONT END
+RETURNS boolean AS $$
+DECLARE
+success BOOLEAN DEFAULT FALSE;
+criteria_report_id INTEGER;
+BEGIN
+
+--CREATE CRITERIA REPORT
+SELECT res_ca_create_report_1_result_4_criteria_report() INTO criteria_report_id;
+PERFORM res_report_1_result_4_rpt_healthplan_type_pharmacy(criteria_report_id);
+success:=true;
+return success;
+END
+$$ LANGUAGE plpgsql;
