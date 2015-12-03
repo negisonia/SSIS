0333f567946547f4490ae969d332a5fc3999f1f5
vsansilvestregap <vsansilvestre@wearegap.com>
2015-12-03 13:26:54 -0600

    adding custom account 5

--- Common/admin/test_data_custom_accounts.sql
+++ Common/admin/test_data_custom_accounts.sql
@@ -13,6 +13,7 @@ PERFORM create_custom_account('Custom_Account_1', common_get_table_id_by_name(cl
 PERFORM create_custom_account('Custom_Account_2', common_get_table_id_by_name(client, 'client_2'), ARRAY[]::integer[], ARRAY[common_get_table_id_by_name(health_plan, 'health_plan_comm'),common_get_table_id_by_name(health_plan, 'health_plan_hix'),common_get_table_id_by_name(health_plan, 'health_plan_comm_1')],false);
 PERFORM create_custom_account('Custom_Account_3', common_get_table_id_by_name(client, 'client_1'), ARRAY[]::integer[], ARRAY[common_get_table_id_by_name(health_plan, 'health_plan_comm'),common_get_table_id_by_name(health_plan, 'health_plan_bcbs')],false);
 PERFORM create_custom_account('Custom_Account_4', common_get_table_id_by_name(client, 'client_1'), ARRAY[]::integer[], ARRAY[common_get_table_id_by_name(health_plan, 'health_plan_comm'),common_get_table_id_by_name(health_plan, 'health_plan_comm_1'),common_get_table_id_by_name(health_plan, 'health_plan_empl_1'),common_get_table_id_by_name(health_plan, 'health_plan_ma_1')],false);
+PERFORM create_custom_account('Custom_Account_5', common_get_table_id_by_name(client, 'client_1'), ARRAY[]::integer[], ARRAY[common_get_table_id_by_name(health_plan, 'health_plan_comm_1'),common_get_table_id_by_name(health_plan, 'health_plan_ma_1')],false);
 
 success=true;
 return success;
