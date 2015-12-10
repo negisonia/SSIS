82e25bf7c9d86368fb1d52df6605b3092b7db4a2
vsansilvestregap <vsansilvestre@wearegap.com>
2015-12-10 16:22:45 -0600

    adding test 96

---
+++ Restrictions/Scripts/report_data/result_6/res_result_6_test_96_validate_test_data.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION res_result_6_test_96_validate_test_data() -- REPORT FRONT END
+RETURNS boolean AS $$
+DECLARE
+  success BOOLEAN DEFAULT FALSE;
+  criteria_report_id INTEGER;
+BEGIN
+--REPORT#1
+SELECT res_create_result_6_criteria_report() INTO criteria_report_id;
+PERFORM res_result_6_rpt_healthplan_type_pharmacy(criteria_report_id);
+
+success=true;
+return success;
+END
+$$ LANGUAGE plpgsql;
