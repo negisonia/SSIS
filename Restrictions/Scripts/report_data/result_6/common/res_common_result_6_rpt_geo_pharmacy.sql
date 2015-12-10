143d796e620ed704eb387667a1aea0e90ba273df
vsansilvestregap <vsansilvestre@wearegap.com>
2015-12-10 15:30:59 -0600

    adding test 92

---
+++ Restrictions/Scripts/report_data/result_6/common/res_common_result_6_rpt_geo_pharmacy.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION res_common_result_6_rpt_geo_pharmacy(report_id INTEGER) -- REPORT FRONT END
+RETURNS boolean AS $$
+DECLARE
+success BOOLEAN DEFAULT FALSE;
+
+market_type INTEGER := 2;
+pharmacy_benefit_type INTEGER := 1;
+expected_output VARCHAR;
+drug_1_id INTEGER;
+drug_2_id INTEGER;
+BEGIN
+
+SELECT common_get_table_id_by_name('drugs','drug_1') INTO drug_1_id;
+SELECT common_get_table_id_by_name('drugs','drug_2') INTO drug_2_id;
+--MARKET TYPES : 1 COUNTIES , 2 STATES , 3 MSA
+
+--MEDICAL GEO
+--expected_output= '';
+--PERFORM res_rpt_geo_validate_data(report_id, pharmacy_benefit_type, market_type, expected_output);
+
+success=true;
+return success;
+END
+$$ LANGUAGE plpgsql;
