f879856ad54941d3ba188960f6bc4e2b48fbc7c0
vsansilvestre <lema017@gmail.com>
2015-12-15 15:27:42 -0600

    changes to  result 4 tests

--- Restrictions/Scripts/report_data/result_4/common/res_common_report_1_result_4_rpt_provider_details_medical.sql
+++ Restrictions/Scripts/report_data/result_4/common/res_common_report_1_result_4_rpt_provider_details_medical.sql
@@ -15,8 +15,9 @@ SELECT common_get_table_id_by_name('health_plan_types','commercial') INTO commer
 SELECT common_get_table_id_by_name('health_plan_types','hix') INTO hix_plan_type_id;
 
 expected_provider_details_table_output= format('['||
-'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%2$s,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"ST - Single - custom_option_2"}'||
-']',provider_id,hix_plan_type_id);
+'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%2$s,"health_plan_type_name":"commercial","lives":200,"drug_name":"drug_2","criteria_restriction_name":"ST - Single - Unspecified"}'||
+'{"provider_id":%1$s,"benefit_name":"Medical","health_plan_type_id":%3$s,"health_plan_type_name":"hix","lives":100,"drug_name":"drug_2","criteria_restriction_name":"ST - Single - custom_option_2"}'||
+']',provider_id,commercial_plan_type_id,hix_plan_type_id);
 PERFORM res_rpt_provider_details_validate_data(report_id, medical_view, expected_provider_details_table_output);
 
 success=true;
