03519cdc06de6e99371f52dc4f9285c66423d6a1
vsansilvestre <lema017@gmail.com>
2015-12-18 12:56:52 -0600

    adding test 136 137 138 139

--- Restrictions/Scripts/report_data/result_7/res_result_7_test_137_validate_test_data.sql
+++ Restrictions/Scripts/report_data/result_7/res_result_7_test_137_validate_test_data.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION res_result_7_test_135_validate_test_data() -- REPORT FRONT END
+CREATE OR REPLACE FUNCTION res_result_7_test_137_validate_test_data() -- REPORT FRONT END
 RETURNS boolean AS $$
 DECLARE
   success BOOLEAN DEFAULT FALSE;
@@ -6,7 +6,7 @@ DECLARE
 BEGIN
 --REPORT#1
 SELECT res_create_result_7_criteria_report() INTO criteria_report_id;
-PERFORM res_common_result_7_rpt_drugs_table_pharmacy(criteria_report_id);
+PERFORM res_common_result_7_rpt_health_plan_details(criteria_report_id);
 
 success=true;
 return success;
