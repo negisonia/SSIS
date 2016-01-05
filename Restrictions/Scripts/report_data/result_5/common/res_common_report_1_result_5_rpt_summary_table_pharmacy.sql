33b6c613b374e16fc07d213d287c9abb014a9740
vsansilvestre <lema017@gmail.com>
2016-01-05 16:06:40 -0600

    changes  for proper result 5 expectations

--- Restrictions/Scripts/report_data/result_5/common/res_common_report_1_result_5_rpt_summary_table_pharmacy.sql
+++ Restrictions/Scripts/report_data/result_5/common/res_common_report_1_result_5_rpt_summary_table_pharmacy.sql
@@ -8,12 +8,13 @@ BEGIN
 
 --VALIDATE SUMMARY TABLE
 expected_pharmacy_summary_table_output= format('['||
-'{"criteria_report_id":%1$s,"drug_name":"drug_1","benefit_restriction_name":"Pharmacy","benefit_name":"Pharmacy","lives":100,"total_pharmacy_lives":100,"health_plan_count":1,"total_health_plan_count":1,"total_medical_lives":0,"provider_count":0,"total_provider_count":0},'||
-'{"criteria_report_id":%1$s,"drug_name":"drug_2","benefit_restriction_name":"PA","benefit_name":"Pharmacy","lives":100,"total_pharmacy_lives":100,"health_plan_count":1,"total_health_plan_count":1,"total_medical_lives":0,"provider_count":0,"total_provider_count":0},'||
-'{"criteria_report_id":%1$s,"drug_name":"drug_2","benefit_restriction_name":"Pharmacy","benefit_name":"Pharmacy","lives":100,"total_pharmacy_lives":100,"health_plan_count":1,"total_health_plan_count":1,"total_medical_lives":0,"provider_count":0,"total_provider_count":0},'||
-'{"criteria_report_id":%1$s,"drug_name":"drug_2","benefit_restriction_name":"QL","benefit_name":"Pharmacy","lives":100,"total_pharmacy_lives":100,"health_plan_count":1,"total_health_plan_count":1,"total_medical_lives":0,"provider_count":0,"total_provider_count":0},'||
-'{"criteria_report_id":%1$s,"drug_name":"drug_2","benefit_restriction_name":"ST","benefit_name":"Pharmacy","lives":100,"total_pharmacy_lives":100,"health_plan_count":1,"total_health_plan_count":1,"total_medical_lives":0,"provider_count":0,"total_provider_count":0},'||
-'{"criteria_report_id":%1$s,"drug_name":null,"benefit_restriction_name":null,"benefit_name":"Pharmacy","lives":null,"total_pharmacy_lives":100,"health_plan_count":null,"total_health_plan_count":1,"total_medical_lives":null,"provider_count":null,"total_provider_count":null}'||
+'{"criteria_report_id":%1$s,"drug_name":"drug_1","benefit_restriction_name":"PA","benefit_name":"Pharmacy","lives":200,"total_pharmacy_lives":300,"health_plan_count":1,"total_health_plan_count":2,"total_medical_lives":0,"provider_count":0,"total_provider_count":0},'||
+'{"criteria_report_id":%1$s,"drug_name":"drug_1","benefit_restriction_name":"Pharmacy","benefit_name":"Pharmacy","lives":300,"total_pharmacy_lives":300,"health_plan_count":2,"total_health_plan_count":2,"total_medical_lives":0,"provider_count":0,"total_provider_count":0},'||
+'{"criteria_report_id":%1$s,"drug_name":"drug_2","benefit_restriction_name":"PA","benefit_name":"Pharmacy","lives":100,"total_pharmacy_lives":300,"health_plan_count":1,"total_health_plan_count":2,"total_medical_lives":0,"provider_count":0,"total_provider_count":0},'||
+'{"criteria_report_id":%1$s,"drug_name":"drug_2","benefit_restriction_name":"Pharmacy","benefit_name":"Pharmacy","lives":300,"total_pharmacy_lives":300,"health_plan_count":2,"total_health_plan_count":2,"total_medical_lives":0,"provider_count":0,"total_provider_count":0},'||
+'{"criteria_report_id":%1$s,"drug_name":"drug_2","benefit_restriction_name":"QL","benefit_name":"Pharmacy","lives":100,"total_pharmacy_lives":300,"health_plan_count":1,"total_health_plan_count":2,"total_medical_lives":0,"provider_count":0,"total_provider_count":0},'||
+'{"criteria_report_id":%1$s,"drug_name":"drug_2","benefit_restriction_name":"ST","benefit_name":"Pharmacy","lives":300,"total_pharmacy_lives":300,"health_plan_count":2,"total_health_plan_count":2,"total_medical_lives":0,"provider_count":0,"total_provider_count":0},'||
+'{"criteria_report_id":%1$s,"drug_name":null,"benefit_restriction_name":null,"benefit_name":"Pharmacy","lives":null,"total_pharmacy_lives":300,"health_plan_count":null,"total_health_plan_count":2,"total_medical_lives":null,"provider_count":null,"total_provider_count":null}'||
 ']',report_id);
 PERFORM res_rpt_summary_table_validate_data(report_id,expected_pharmacy_summary_table_output,pharmacy_view);
 
