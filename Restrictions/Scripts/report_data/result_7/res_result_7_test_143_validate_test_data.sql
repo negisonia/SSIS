7ed9a821b9bd10eae5d1c2885ee9ca8abfc8cd0c
vsansilvestregap <vsansilvestre@wearegap.com>
2016-01-11 16:24:12 -0600

    comments

--- Restrictions/Scripts/report_data/result_7/res_result_7_test_143_validate_test_data.sql
+++ Restrictions/Scripts/report_data/result_7/res_result_7_test_143_validate_test_data.sql
@@ -12,3 +12,21 @@ success=true;
 return success;
 END
 $$ LANGUAGE plpgsql;
+
+
+--/**
+--review data for summary table  drug 1  pharmacy pa/st
+--we have pa/st  for commercial and employer Health plans
+--
+--we added 100 lives from HLI LIVES  for employer hot as there is no commercial plan type associated to a commercial hpt so we look for provider 11 middlesex hi lives and add this lives to the 500 total
+--
+--
+--medical st  drug 1  100  is not coming in summary table and it should come
+--
+--
+--pharmacy  drugs table:
+--
+--first line difference should be 200
+--commercial drug 1  indications 1 provider 1 : single custom option one
+--employer single drug 3 drug 1 provider 11 indication 1
+--**/
